7e84b086bb7160820945e13683a47753c2382f13
Paul Colomiets <paul@colomiets.name>
2018-02-03 02:32:18 +0200

    Tokenizer and validator for integer and floating-point tokens

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -36,26 +36,8 @@ impl<'a> StreamOnce for TokenStream<'a> {
     fn uncons<E>(&mut self) -> Result<Self::Item, E>
         where E: StreamError<Self::Item, Self::Range>
     {
-        use self::Kind::*;
-        let (kind, len) = {
-            let mut iter = self.buf[self.off..].chars();
-            loop {
-                let cur_char = match iter.next() {
-                    Some(x) => x,
-                    None => return Err(E::end_of_input()),
-                };
-                match cur_char {
-                    '!' | '$' | ':' | '=' | '@' | '|' |
-                    '(' | ')' | '[' | ']' | '{' | '}' => {
-                        break (Punctuator, 1);
-                    }
-                    // TODO(tailhook) punctuator '...'
-                    _ => return Err(E::unexpected_message(
-                        format_args!("unexpected character {:?}", cur_char))),
-                }
-            }
-        };
-        let value = &self.buf[..len];
+        let (kind, len) = self.peek_token()?;
+        let value = &self.buf[self.off..][..len];
         self.update_position(len);
         self.skip_whitespace();
         Ok(Token { kind, value })
@@ -68,6 +50,41 @@ impl<'a> Positioned for TokenStream<'a> {
     }
 }
 
+// NOTE: we expect that first character is always digit or minus, as returned
+// by tokenizer
+fn check_int(value: &str) -> bool {
+    return value == "0" || value == "-0" ||
+       (!value.starts_with("0") && value != "-" && !value.starts_with("-0")
+       && value[1..].chars().all(|x| x >= '0' && x <= '9'));
+}
+
+fn check_dec(value: &str) -> bool {
+    return value.len() > 0 && value.chars().all(|x| x >= '0' && x <= '9');
+}
+
+fn check_exp(value: &str) -> bool {
+    return (value.starts_with('-') || value.starts_with('+')) &&
+        value.len() >= 2 &&
+        value[1..].chars().all(|x| x >= '0' && x <= '9');
+}
+
+fn check_float(value: &str, exponent: Option<usize>, real: Option<usize>)
+    -> bool
+{
+    match (exponent, real) {
+        (Some(e), Some(r)) if e < r => return false,
+        (Some(e), Some(r))
+        => check_int(&value[..r]) &&
+           check_dec(&value[r+1..e]) &&
+           check_exp(&value[e+1..]),
+        (Some(e), None)
+        => check_int(&value[..e]) && check_exp(&value[e+1..]),
+        (None, Some(r))
+        => check_int(&value[..r]) && check_dec(&value[r+1..]),
+        (None, None) => unreachable!(),
+    }
+}
+
 impl<'a> TokenStream<'a> {
     pub fn new(s: &str) -> TokenStream {
         let mut me = TokenStream {
@@ -77,6 +94,79 @@ impl<'a> TokenStream<'a> {
         };
         me.skip_whitespace();
         return me;
+    }
+    fn peek_token<E>(&self) -> Result<(Kind, usize), E>
+        where E: StreamError<Token<'a>, Token<'a>>
+    {
+        use self::Kind::*;
+        let mut iter = self.buf[self.off..].char_indices();
+        let cur_char = match iter.next() {
+            Some((_, x)) => x,
+            None => return Err(E::end_of_input()),
+        };
+        match cur_char {
+            '!' | '$' | ':' | '=' | '@' | '|' |
+            '(' | ')' | '[' | ']' | '{' | '}' => {
+                return Ok((Punctuator, 1));
+            }
+            '.' => {
+                if iter.as_str().starts_with("..") {
+                    return Ok((Punctuator, 3))
+                } else {
+                    return Err(E::unexpected_message(
+                        format_args!("bare dot {:?} is not suppored, \
+                            only \"...\"", cur_char)));
+                }
+            }
+            '_' | 'a'...'z' | 'A'...'Z' => {
+                while let Some((idx, cur_char)) = iter.next() {
+                    match cur_char {
+                        '_' | 'a'...'z' | 'A'...'Z' | '0'...'9' => continue,
+                        _ => {
+                            return Ok((Name, idx));
+                        }
+                    }
+                }
+                return Ok((Name, self.buf.len() - self.off));
+            }
+            '-' | '0'...'9' => {
+                let mut exponent = None;
+                let mut real = None;
+                let len = loop {
+                    let (idx, cur_char) = match iter.next() {
+                        Some(pair) => pair,
+                        None => break self.buf.len() - self.off,
+                    };
+                    match cur_char {
+                        // just scan for now, will validate later on
+                        ' ' | '\n' | '\r' | '\t' |
+                        '!' | '$' | ':' | '=' | '@' | '|' |
+                        '(' | ')' | '[' | ']' | '{' | '}'
+                        => break idx,
+                        '.' => real = Some(idx),
+                        'e' | 'E' => exponent = Some(idx),
+                        _ => {},
+                    }
+                };
+                if exponent.is_some() || real.is_some() {
+                    let value = &self.buf[self.off..][..len];
+                    if !check_float(value, exponent, real) {
+                        return Err(E::unexpected_message(
+                            format_args!("unsupported float {:?}", value)));
+                    }
+                    return Ok((FloatValue, len));
+                } else {
+                    let value = &self.buf[self.off..][..len];
+                    if !check_int(value) {
+                        return Err(E::unexpected_message(
+                            format_args!("unsupported integer {:?}", value)));
+                    }
+                    return Ok((IntValue, len));
+                }
+            }
+            _ => return Err(E::unexpected_message(
+                format_args!("unexpected character {:?}", cur_char))),
+        }
     }
     fn skip_whitespace(&mut self) {
         let num = {
@@ -145,7 +235,7 @@ mod test {
             match s.uncons::<UnexpectedParse>() {
                 Ok(x) => r.push(x.value),
                 Err(UnexpectedParse::Eoi) => break,
-                Err(e) => panic!("Parse error at: {}, {}", s.position(), e),
+                Err(e) => panic!("Parse error at {}: {}", s.position(), e),
             }
         }
         return r;
@@ -157,7 +247,7 @@ mod test {
             match s.uncons::<UnexpectedParse>() {
                 Ok(x) => r.push(x.kind),
                 Err(UnexpectedParse::Eoi) => break,
-                Err(e) => panic!("Parse error at: {}, {}", s.position(), e),
+                Err(e) => panic!("Parse error at {}: {}", s.position(), e),
             }
         }
         return r;
@@ -182,4 +272,82 @@ mod test {
             object { field }
         }"), ["query", "Query", "{", "object", "{", "field", "}", "}"]);
     }
+
+    #[test]
+    fn fragment() {
+        assert_eq!(tok_str("a { ...b }"), ["a", "{", "...", "b", "}"]);
+    }
+
+    #[test]
+    fn int() {
+        assert_eq!(tok_str("0"), ["0"]);
+        assert_eq!(tok_typ("0"), [IntValue]);
+        assert_eq!(tok_str("-0"), ["-0"]);
+        assert_eq!(tok_typ("-0"), [IntValue]);
+        assert_eq!(tok_str("-1"), ["-1"]);
+        assert_eq!(tok_typ("-1"), [IntValue]);
+        assert_eq!(tok_str("-132"), ["-132"]);
+        assert_eq!(tok_typ("-132"), [IntValue]);
+        assert_eq!(tok_str("132"), ["132"]);
+        assert_eq!(tok_typ("132"), [IntValue]);
+        assert_eq!(tok_str("a(x: 10) { b }"),
+            ["a", "(", "x", ":", "10", ")", "{", "b", "}"]);
+        assert_eq!(tok_typ("a(x: 10) { b }"),
+            [Name, Punctuator, Name, Punctuator, IntValue, Punctuator,
+                Punctuator, Name, Punctuator]);
+    }
+
+    // TODO(tailhook) fix errors in parser and check error message
+    #[test] #[should_panic] fn zero_int() { tok_str("01"); }
+    #[test] #[should_panic] fn zero_int4() { tok_str("00001"); }
+    #[test] #[should_panic] fn minus_int() { tok_str("-"); }
+    #[test] #[should_panic] fn minus_zero_int() { tok_str("-01"); }
+    #[test] #[should_panic] fn minus_zero_int4() { tok_str("-00001"); }
+    #[test] #[should_panic] fn letters_int() { tok_str("0bbc"); }
+
+    #[test]
+    fn float() {
+        assert_eq!(tok_str("0.0"), ["0.0"]);
+        assert_eq!(tok_typ("0.0"), [FloatValue]);
+        assert_eq!(tok_str("-0.0"), ["-0.0"]);
+        assert_eq!(tok_typ("-0.0"), [FloatValue]);
+        assert_eq!(tok_str("-1.0"), ["-1.0"]);
+        assert_eq!(tok_typ("-1.0"), [FloatValue]);
+        assert_eq!(tok_str("-1.023"), ["-1.023"]);
+        assert_eq!(tok_typ("-1.023"), [FloatValue]);
+        assert_eq!(tok_str("-132.0"), ["-132.0"]);
+        assert_eq!(tok_typ("-132.0"), [FloatValue]);
+        assert_eq!(tok_str("132.0"), ["132.0"]);
+        assert_eq!(tok_typ("132.0"), [FloatValue]);
+        assert_eq!(tok_str("0e+0"), ["0e+0"]);
+        assert_eq!(tok_typ("0e+0"), [FloatValue]);
+        assert_eq!(tok_str("0.0e+0"), ["0.0e+0"]);
+        assert_eq!(tok_typ("0.0e+0"), [FloatValue]);
+        assert_eq!(tok_str("-0e+0"), ["-0e+0"]);
+        assert_eq!(tok_typ("-0e+0"), [FloatValue]);
+        assert_eq!(tok_str("-1e+0"), ["-1e+0"]);
+        assert_eq!(tok_typ("-1e+0"), [FloatValue]);
+        assert_eq!(tok_str("-132e+0"), ["-132e+0"]);
+        assert_eq!(tok_typ("-132e+0"), [FloatValue]);
+        assert_eq!(tok_str("132e+0"), ["132e+0"]);
+        assert_eq!(tok_typ("132e+0"), [FloatValue]);
+        assert_eq!(tok_str("a(x: 10.0) { b }"),
+            ["a", "(", "x", ":", "10.0", ")", "{", "b", "}"]);
+        assert_eq!(tok_typ("a(x: 10.0) { b }"),
+            [Name, Punctuator, Name, Punctuator, FloatValue, Punctuator,
+                Punctuator, Name, Punctuator]);
+    }
+
+    // TODO(tailhook) fix errors in parser and check error message
+    #[test] #[should_panic] fn no_int_float() { tok_str(".0"); }
+    #[test] #[should_panic] fn no_int_float1() { tok_str(".1"); }
+    #[test] #[should_panic] fn zero_float() { tok_str("01.0"); }
+    #[test] #[should_panic] fn zero_float4() { tok_str("00001.0"); }
+    #[test] #[should_panic] fn minus_float() { tok_str("-.0"); }
+    #[test] #[should_panic] fn minus_zero_float() { tok_str("-01.0"); }
+    #[test] #[should_panic] fn minus_zero_float4() { tok_str("-00001.0"); }
+    #[test] #[should_panic] fn letters_float() { tok_str("0bbc.0"); }
+    #[test] #[should_panic] fn letters_float2() { tok_str("0.bbc"); }
+    #[test] #[should_panic] fn letters_float3() { tok_str("0.bbce0"); }
+    #[test] #[should_panic] fn no_exp_sign_float() { tok_str("0e0"); }
 }
