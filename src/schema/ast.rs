a96063ea6b22dbef2527570d9bf10fd94ab81e93
Paul Colomiets <paul@colomiets.name>
2018-02-11 03:04:08 +0200

    Implement as_str()/from_str() for DirectiveLocation

--- src/schema/ast.rs
+++ src/schema/ast.rs
@@ -1,3 +1,4 @@
+use std::str::FromStr;
 use std::collections::HashSet;
 
 pub use common::{Directive, Type, Name, Value};
@@ -164,25 +165,25 @@ pub struct InputObjectTypeExtension {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum DirectiveLocation {
     // executable
-    QUERY,
-    MUTATION,
-    SUBSCRIPTION,
-    FIELD,
-    FRAGMENT_DEFINITION,
-    FRAGMENT_SPREAD,
-    INLINE_FRAGMENT,
+    Query,
+    Mutation,
+    Subscription,
+    Field,
+    FragmentDefinition,
+    FragmentSpread,
+    InlineFragment,
     // type_system
-    SCHEMA,
-    SCALAR,
-    OBJECT,
-    FIELD_DEFINITION,
-    ARGUMENT_DEFINITION,
-    INTERFACE,
-    UNION,
-    ENUM,
-    ENUM_VALUE,
-    INPUT_OBJECT,
-    INPUT_FIELD_DEFINITION,
+    Schema,
+    Scalar,
+    Object,
+    FieldDefinition,
+    ArgumentDefinition,
+    Interface,
+    Union,
+    Enum,
+    EnumValue,
+    InputObject,
+    InputFieldDefinition,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -192,4 +193,114 @@ pub struct DirectiveDefinition {
     arguments: Vec<InputValue>,
     // TODO(tailhook) probably convert to a bitset
     locations: HashSet<DirectiveLocation>,
+}
+
+impl DirectiveLocation {
+    /// Returns graphql syntax compatible name of the directive
+    pub fn as_str(&self) -> &'static str {
+        use self::DirectiveLocation::*;
+        match *self {
+            Query => "QUERY",
+            Mutation => "MUTATION",
+            Subscription => "SUBSCRIPTION",
+            Field => "FIELD",
+            FragmentDefinition => "FRAGMENT_DEFINITION",
+            FragmentSpread => "FRAGMENT_SPREAD",
+            InlineFragment => "INLINE_FRAGMENT",
+            Schema => "SCHEMA",
+            Scalar => "SCALAR",
+            Object => "OBJECT",
+            FieldDefinition => "FIELD_DEFINITION",
+            ArgumentDefinition => "ARGUMENT_DEFINITION",
+            Interface => "INTERFACE",
+            Union => "UNION",
+            Enum => "ENUM",
+            EnumValue => "ENUM_VALUE",
+            InputObject => "INPUT_OBJECT",
+            InputFieldDefinition => "INPUT_FIELD_DEFINITION",
+        }
+    }
+    /// Returns true if this location is for queries (execution)
+    pub fn is_query(&self) -> bool {
+        use self::DirectiveLocation::*;
+        match *self {
+            Query => true,
+            Mutation => true,
+            Subscription => true,
+            Field => true,
+            FragmentDefinition => true,
+            FragmentSpread => true,
+            InlineFragment => true,
+            Schema => false,
+            Scalar => false,
+            Object => false,
+            FieldDefinition => false,
+            ArgumentDefinition => false,
+            Interface => false,
+            Union => false,
+            Enum => false,
+            EnumValue => false,
+            InputObject => false,
+            InputFieldDefinition => false,
+        }
+    }
+    /// Returns true if this location is for schema
+    pub fn is_schema(&self) -> bool {
+        use self::DirectiveLocation::*;
+        match *self {
+            Query => false,
+            Mutation => false,
+            Subscription => false,
+            Field => false,
+            FragmentDefinition => false,
+            FragmentSpread => false,
+            InlineFragment => false,
+            Schema => true,
+            Scalar => true,
+            Object => true,
+            FieldDefinition => true,
+            ArgumentDefinition => true,
+            Interface => true,
+            Union => true,
+            Enum => true,
+            EnumValue => true,
+            InputObject => true,
+            InputFieldDefinition => true,
+        }
+    }
+}
+
+#[derive(Debug, Fail)]
+#[fail(display = "invalid directive location")]
+pub struct InvalidDirectiveLocation;
+
+
+impl FromStr for DirectiveLocation {
+    type Err = InvalidDirectiveLocation;
+    fn from_str(s: &str) -> Result<DirectiveLocation, InvalidDirectiveLocation>
+    {
+        use self::DirectiveLocation::*;
+        let val = match s {
+            "QUERY" => Query,
+            "MUTATION" => Mutation,
+            "SUBSCRIPTION" => Subscription,
+            "FIELD" => Field,
+            "FRAGMENT_DEFINITION" => FragmentDefinition,
+            "FRAGMENT_SPREAD" => FragmentSpread,
+            "INLINE_FRAGMENT" => InlineFragment,
+            "SCHEMA" => Schema,
+            "SCALAR" => Scalar,
+            "OBJECT" => Object,
+            "FIELD_DEFINITION" => FieldDefinition,
+            "ARGUMENT_DEFINITION" => ArgumentDefinition,
+            "INTERFACE" => Interface,
+            "UNION" => Union,
+            "ENUM" => Enum,
+            "ENUM_VALUE" => EnumValue,
+            "INPUT_OBJECT" => InputObject,
+            "INPUT_FIELD_DEFINITION" => InputFieldDefinition,
+            _ => return Err(InvalidDirectiveLocation),
+        };
+        return Ok(val);
+    }
 }
