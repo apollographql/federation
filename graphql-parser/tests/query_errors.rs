f9c98510cd518b4f65d2ad5e0ea05353de1c1bdb
jbaxleyiii <james@apollographql.com>
2020-04-09 19:46:05 -0400

    Use PathBuf to allow tests to work on windows
    PathBuf normalizes the seperator of the path to be platform specific (I
    think) allowing just this test suite to run on windows (I expect others
    to fail)

--- graphql-parser/tests/query_errors.rs
+++ graphql-parser/tests/query_errors.rs
@@ -3,12 +3,15 @@ extern crate graphql_parser;
 
 use std::io::Read;
 use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
 
 use graphql_parser::parse_query;
 
 fn test_error(filename: &str) {
     let mut buf = String::with_capacity(1024);
     let path = format!("tests/query_errors/{}.txt", filename);
+    let path = PathBuf::from(&path);
     let mut f = File::open(&path).unwrap();
     f.read_to_string(&mut buf).unwrap();
     let mut iter = buf.splitn(2, "\n---\n");
