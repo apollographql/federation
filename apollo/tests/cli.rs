420d7dc059dd8d5f33880c0df97f234485c1065d
Jake <dawkinsjh@gmail.com>
2020-04-14 15:07:16 -0400

    setup command and test structure
    - move test fixtures to apollo/tests/fixtures
    - add in structopt
    - set up command module

--- apollo/tests/cli.rs
+++ apollo/tests/cli.rs
@@ -3,29 +3,32 @@ use predicates::prelude::*;
 use std::process::Command; // Run programs // Used for writing assertions
 
 #[test]
-fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+fn no_command_used() -> Result<(), Box<dyn std::error::Error>> {
     let mut apollo = Command::cargo_bin("apollo").unwrap();
 
     apollo
         .assert()
-        .success()
-        .stdout(predicate::str::contains("USAGE"));
+        .code(1)
+        .stderr(predicate::str::contains("USAGE"));
 
     Ok(())
 }
 
 #[test]
-fn print_doesnt() -> Result<(), Box<dyn std::error::Error>> {
+fn prints_types() -> Result<(), Box<dyn std::error::Error>> {
     let mut apollo = Command::cargo_bin("apollo").unwrap();
 
     apollo
         .arg("print")
-        .arg("../samples/basic.gql")
+        // current_dir is `apollo`, not `apollo/tests`
+        .arg("./tests/fixtures/basic.graphql")
+        .arg("./tests/fixtures/my-enum.graphql")
         .assert()
         .success()
         .stdout(predicate::str::contains("type Query"))
         .stdout(predicate::str::contains("type User"))
-        .stdout(predicate::str::contains("type Song"));
+        .stdout(predicate::str::contains("  favoriteSongs: [Song]"))
+        .stdout(predicate::str::contains("enum MyEnum"));
 
     Ok(())
 }
