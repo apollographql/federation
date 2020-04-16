57a422350fdabf1fb938a344fd9dfd2e8cb3ddf7
Ashi Krishnan <i@ashi.io>
2020-04-15 19:04:36 -0600

    Create a named struct for each command.

--- apollo/src/command_config.rs
+++ apollo/src/command_config.rs
@@ -1,13 +1,21 @@
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
 /// The [Experimental] Apollo CLI, for supporting all your graphql needs :)
 pub enum Apollo {
-  #[allow(non_camel_case_types)]
-  /// parse and pretty print schemas to stdout
-  print {
-    /// the input filepath to pretty print
-    #[structopt(parse(from_os_str))]
-    file: std::vec::Vec<std::path::PathBuf>,
-  },
+  Print(Print),
+}
+
+#[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
+/// parse and pretty print schemas to stdout
+pub struct Print {
+  #[structopt(short = "h", long)]
+  /// suppress headers when printing multiple files
+  pub no_headers: bool,
+
+  #[structopt(parse(from_os_str))]
+  /// schemas to print
+  pub file: std::vec::Vec<std::path::PathBuf>,
 }
--- apollo/src/commands/print/mod.rs
+++ apollo/src/commands/print/mod.rs
@@ -1,16 +1,21 @@
 use graphql_parser::parse_schema;
 use std::fs;
 
-pub fn print(files: &mut dyn Iterator<Item = std::path::PathBuf>) {
-  println!("{:?}", std::env::current_dir());
-  files.for_each(move |file| {
+use crate::command_config::Print;
+
+pub fn print(opts: &Print) {
+  opts.file.iter().for_each(move |file| {
     let schema = fs::read_to_string(file).expect("reading schema");
     let doc = parse_schema::<&str>(&schema).expect("parsing schema");
+    if !opts.no_headers && opts.file.len() > 1 {
+      println!("# {}", file.to_str().expect("filename"));
+    }
     println!("{}", doc);
   })
 }
 
 #[test]
 fn does_not_fail_with_no_files() {
-  print(&mut vec![].into_iter());
+  print(&Print { file: vec![], no_headers: false });
+  print(&Print { file: vec![], no_headers: true });
 }
