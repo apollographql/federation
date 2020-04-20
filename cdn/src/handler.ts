7df22df3f8e00563a16eed81435f3f96dfa7ad56
jbaxleyiii <james@apollographql.com>
2020-04-20 12:59:48 -0400

    remove extra promise wrappers now that we are async

--- cdn/src/handler.ts
+++ cdn/src/handler.ts
@@ -23,9 +23,7 @@ const SUPPORTED_METHODS = ["GET", "HEAD"]
 export async function handleRequest(event: FetchEvent): Promise<Response> {
   try {
     if (!SUPPORTED_METHODS.includes(event.request.method)) {
-      return Promise.resolve(
-        new Response(`This proxy only supports the following request types: ${SUPPORTED_METHODS.join(", ")}`, { status: 500 })
-      )
+      return new Response(`This proxy only supports the following request types: ${SUPPORTED_METHODS.join(", ")}`, { status: 500 })
     }
 
     const url = new URL(event.request.url);
@@ -69,9 +67,7 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
     }
   } catch (e) {
     event.waitUntil(log(e, event.request));
-    return Promise.resolve(
-      new Response(e.message || e.toString(), { status: 500 })
-    );
+    return new Response(e.message || e.toString(), { status: 500 })
   }
 }
 
