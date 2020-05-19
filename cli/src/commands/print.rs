610c77e64ec9ac775af4a6ff0c1220330a479d59
Ashi Krishnan <i@ashi.io>
2020-05-19 16:59:01 -0600

    Fix failing test.

--- cli/src/commands/print.rs
+++ cli/src/commands/print.rs
@@ -27,7 +27,7 @@ impl Command for Print {
         let printing_headers = !self.no_headers && self.files.len() > 1;
         self.files.iter().for_each(move |file| {
             let schema = fs::read_to_string(file).expect("reading schema");
-            let doc = parse_schema::<&str>(&schema).expect("parsing schema");
+            let doc = parse_schema(&schema).expect("parsing schema");
             if printing_headers {
                 println!("# {}", file.to_str().expect("filename"));
             }
