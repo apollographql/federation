e5e833fc69af9df777c3a11e30e1343e9ba387ca
Ran Magen <ran@apollographql.com>
2020-05-29 13:13:51 -0700

    Introduce code generation from .graphql files for making queries

---
+++ cli/src/client/mod.rs
@@ -0,0 +1,44 @@
+use graphql_client::{GraphQLQuery, Response};
+use http::Uri;
+use reqwest::blocking;
+use std::error::Error;
+
+mod queries;
+
+static PROD_GQL_API_URL: &str = "https://engine-graphql.apollographql.com/api/graphql";
+
+pub struct Client {
+    personal_api_key: String,
+    uri: Uri,
+    reqwest: blocking::Client,
+}
+
+impl Client {
+    fn from(personal_api_key: String) -> Client {
+        Client {
+            personal_api_key,
+            uri: PROD_GQL_API_URL.parse::<Uri>().unwrap(),
+            reqwest: blocking::Client::new(),
+        }
+    }
+
+    fn send<Q: GraphQLQuery>(
+        &self,
+        variables: Q::Variables,
+    ) -> Result<Response<Q::ResponseData>, Box<dyn Error>> {
+        let request_body = Q::build_query(variables);
+
+        let res = self
+            .reqwest
+            .post(&self.uri.to_string())
+            .header("Content-Type", "application/json")
+            .header("apollographql-client-name", "internal-apollo-cli")
+            .header("apollographql-client-version", "0.1")
+            .header("x-api-key", &self.personal_api_key)
+            .json(&request_body)
+            .send()?;
+
+        let response_body: Response<Q::ResponseData> = res.json()?;
+        Ok(response_body)
+    }
+}
