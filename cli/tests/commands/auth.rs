5b6a34704b687e1a48c861e35c53e874ad1616a2
AlexanderMann <mannimal09@gmail.com>
2020-06-01 13:18:09 -0500

    Housekeeping: RustFmt on auth commands tests

--- cli/tests/commands/auth.rs
+++ cli/tests/commands/auth.rs
@@ -1,7 +1,7 @@
 use crate::utils::get_cli;
-use std::process::Stdio;
-use std::io::Write;
 use config::Config;
+use std::io::Write;
+use std::process::Stdio;
 
 #[test]
 fn writes_auth_token() {
@@ -10,79 +10,102 @@ fn writes_auth_token() {
 
     let test_api_key = "test_key";
 
-    let mut cli_spawn = cli.arg("auth")
+    let mut cli_spawn = cli
+        .arg("auth")
         .arg("setup")
         .stdin(Stdio::piped())
         .spawn()
         .unwrap();
 
-
-    cli_spawn.stdin.as_mut().unwrap().write_all(test_api_key.as_ref()).unwrap();
+    cli_spawn
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(test_api_key.as_ref())
+        .unwrap();
     cli_spawn.wait().unwrap();
 
-    let config_path = test_command.home_dir.path().join(".apollo").join("config.toml");
+    let config_path = test_command
+        .home_dir
+        .path()
+        .join(".apollo")
+        .join("config.toml");
     let mut s = Config::new();
-    s.merge(config::File::with_name(config_path.to_str().unwrap())).unwrap();
+    s.merge(config::File::with_name(config_path.to_str().unwrap()))
+        .unwrap();
     assert_eq!(test_api_key, s.get::<String>("api_key").unwrap());
 }
 
 #[test]
 fn verify_overwrite_auth_token() {
     let mut test_command = get_cli();
-    let cli = test_command.command
-        .arg("auth")
-        .arg("setup");
+    let cli = test_command.command.arg("auth").arg("setup");
     let test_api_key = "test_key";
 
     {
-        let mut cli_spawn = cli
-            .stdin(Stdio::piped())
-            .spawn()
+        let mut cli_spawn = cli.stdin(Stdio::piped()).spawn().unwrap();
+
+        cli_spawn
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all("foo-bar".as_ref())
             .unwrap();
-
-
-        cli_spawn.stdin.as_mut().unwrap().write_all("foo-bar".as_ref()).unwrap();
         cli_spawn.wait().unwrap();
     }
 
     {
-        let mut cli_spawn = cli
-            .stdin(Stdio::piped())
-            .spawn()
-            .unwrap();
+        let mut cli_spawn = cli.stdin(Stdio::piped()).spawn().unwrap();
 
-        cli_spawn.stdin.as_mut().unwrap().write_all("y\ntest_key".as_ref()).unwrap();
+        cli_spawn
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all("test_key".as_ref())
+            .unwrap();
         cli_spawn.wait().unwrap();
     }
 
-    let config_path = test_command.home_dir.path().join(".apollo").join("config.toml");
+    let config_path = test_command
+        .home_dir
+        .path()
+        .join(".apollo")
+        .join("config.toml");
     let mut s = Config::new();
-    s.merge(config::File::with_name(config_path.to_str().unwrap())).unwrap();
+    s.merge(config::File::with_name(config_path.to_str().unwrap()))
+        .unwrap();
     assert_eq!(test_api_key, s.get::<String>("api_key").unwrap());
 }
 
 #[test]
 fn verify_environment_variables_for_config() {
     let mut test_command = get_cli();
-    let cli = test_command.command
+    let cli = test_command
+        .command
         .arg("auth")
         .arg("setup")
         .env("APOLLO__API_KEY", "I EXIST");
     let test_api_key = "test_key";
 
     {
-        let mut cli_spawn = cli
-            .stdin(Stdio::piped())
-            .spawn()
+        let mut cli_spawn = cli.stdin(Stdio::piped()).spawn().unwrap();
+
+        cli_spawn
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all("n".as_ref())
             .unwrap();
-
-
-        cli_spawn.stdin.as_mut().unwrap().write_all("n".as_ref()).unwrap();
         cli_spawn.wait().unwrap();
     }
 
-    let config_path = test_command.home_dir.path().join(".apollo").join("config.toml");
+    let config_path = test_command
+        .home_dir
+        .path()
+        .join(".apollo")
+        .join("config.toml");
     let mut s = Config::new();
-    s.merge(config::File::with_name(config_path.to_str().unwrap())).unwrap();
+    s.merge(config::File::with_name(config_path.to_str().unwrap()))
+        .unwrap();
     assert!(s.get::<String>("api_key").is_err());
 }
