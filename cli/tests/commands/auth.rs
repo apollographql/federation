e7d8e8b2da512883618d49373903a83a36b7a05c
AlexanderMann <mannimal09@gmail.com>
2020-06-02 17:20:33 -0500

    Feature: Tests for whitespace only keys are considered invalid

--- cli/tests/commands/auth.rs
+++ cli/tests/commands/auth.rs
@@ -37,6 +37,39 @@ fn writes_auth_token() {
 }
 
 #[test]
+fn writes_auth_token_trims_whitespace() {
+    let test_command = get_cli();
+    let mut cli = test_command.command;
+
+    let test_api_key = "test_key";
+
+    let mut cli_spawn = cli
+        .arg("auth")
+        .arg("setup")
+        .stdin(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    cli_spawn
+        .stdin
+        .as_mut()
+        .unwrap()
+        .write_all(format!("    {}  ", test_api_key).as_ref())
+        .unwrap();
+    cli_spawn.wait().unwrap();
+
+    let config_path = test_command
+        .home_dir
+        .path()
+        .join(".apollo")
+        .join("config.toml");
+    let mut s = Config::new();
+    s.merge(config::File::with_name(config_path.to_str().unwrap()))
+        .unwrap();
+    assert_eq!(test_api_key, s.get::<String>("api_key").unwrap());
+}
+
+#[test]
 fn verify_overwrite_auth_token() {
     let mut test_command = get_cli();
     let cli = test_command.command.arg("auth").arg("setup");
@@ -75,6 +108,45 @@ fn verify_overwrite_auth_token() {
     s.merge(config::File::with_name(config_path.to_str().unwrap()))
         .unwrap();
     assert_eq!(test_api_key, s.get::<String>("api_key").unwrap());
+}
+
+#[test]
+fn non_zero_exit_code_when_empty_string_key_entered() {
+    let test_command = get_cli();
+    let mut cli = test_command.command;
+    {
+        let mut cli_spawn = cli
+            .arg("auth")
+            .arg("setup")
+            .stdin(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        cli_spawn
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all("".as_ref())
+            .unwrap();
+        assert_ne!(cli_spawn.wait().unwrap().success(), true);
+    }
+
+    {
+        let mut cli_spawn = cli
+            .arg("auth")
+            .arg("setup")
+            .stdin(Stdio::piped())
+            .spawn()
+            .unwrap();
+
+        cli_spawn
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all("         ".as_ref())
+            .unwrap();
+        assert_ne!(cli_spawn.wait().unwrap().success(), true);
+    }
 }
 
 #[test]
