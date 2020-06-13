81fbafe190406a875740ed5dec8255a521d79c44
Ran Magen <ran@apollographql.com>
2020-06-13 14:11:05 -0700

    Fix tests to be in a module, use `assert_eq!`, and a random hash that is not just `'a'`s.

--- cli/src/commands/schema/mod.rs
+++ cli/src/commands/schema/mod.rs
@@ -10,6 +10,7 @@ pub use get::Get;
 use regex::Regex;
 pub use store::Store;
 
+#[derive(PartialEq, Debug)]
 enum SchemaRef {
     SchemaVariantRef { graph_id: String, variant: String },
     SchemaHashRef { graph_id: String, hash: String },
@@ -57,47 +58,47 @@ pub enum Schema {
 #[cfg(test)]
 mod tests {
     use super::{parse_schema_ref, SchemaRef};
+    use rand;
+    use rand::seq::SliceRandom;
+
+    fn hash() -> String {
+        let chars = vec![
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+        ];
+
+        (0..64)
+            .map(|_| chars.choose(&mut rand::thread_rng()).unwrap())
+            .collect()
+    }
 
     #[test]
     fn parse_variant_ref() {
         let output = parse_schema_ref("graphID@variant");
-        assert!(match output {
-            Ok(schema_ref) => {
-                match schema_ref {
-                    SchemaRef::SchemaVariantRef { graph_id, variant } => {
-                        graph_id == "graphID" && variant == "variant"
-                    }
-                    SchemaRef::SchemaHashRef { .. } => false,
-                }
+        assert_eq!(
+            output.unwrap(),
+            SchemaRef::SchemaVariantRef {
+                graph_id: "graphID".to_string(),
+                variant: "variant".to_string()
             }
-            Err { .. } => false,
-        })
+        );
     }
 
     #[test]
     fn parse_hash_ref() {
-        let output = parse_schema_ref(
-            "graphID#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-        );
-        assert!(match output {
-            Ok(schema_ref) => {
-                match schema_ref {
-                    SchemaRef::SchemaVariantRef { .. } => false,
-                    SchemaRef::SchemaHashRef { graph_id, hash } => graph_id == "graphID"
-                        && hash
-                            == "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-                }
+        let h = hash();
+        let output = parse_schema_ref(format!("graphID#{}", h).as_str());
+        assert_eq!(
+            output.unwrap(),
+            SchemaRef::SchemaHashRef {
+                graph_id: "graphID".to_string(),
+                hash: h
             }
-            Err { .. } => false,
-        })
+        );
     }
 
     #[test]
     fn parse_fail() {
         let output = parse_schema_ref("bad schema ref");
-        assert!(match output {
-            Ok { .. } => false,
-            Err { .. } => true,
-        })
+        assert!(output.is_err());
     }
 }
--- cli/tests/commands/schema/get.rs
+++ cli/tests/commands/schema/get.rs
@@ -1,10 +1,22 @@
 use crate::utils::{add_mock_graphql, get_cli};
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use rand;
+use rand::seq::SliceRandom;
 use serde_json::Value;
 use std::str;
 use wiremock::matchers::method;
 use wiremock::{Request, ResponseTemplate};
+
+fn hash() -> String {
+    let chars = vec![
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
+    ];
+
+    (0..64)
+        .map(|_| chars.choose(&mut rand::thread_rng()).unwrap())
+        .collect()
+}
 
 #[test]
 fn schem_ref_error() {
@@ -32,14 +44,15 @@ async fn gets_schema_by_hash() {
         }"#,
     );
 
+    let h = hash();
+    let h_clone = h.clone();
+
     let matcher = move |request: &Request| {
         let body: Value = serde_json::from_str(str::from_utf8(&request.body).unwrap()).unwrap();
         let variables = &body["variables"];
-        assert!(variables["graphId"] == "test");
+        assert_eq!(variables["graphId"], "test");
         assert!(variables["variant"].is_null());
-        assert!(
-            variables["hash"] == "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        );
+        assert_eq!(variables["hash"], h);
         true
     };
 
@@ -48,7 +61,7 @@ async fn gets_schema_by_hash() {
     cli.command
         .arg("schema")
         .arg("get")
-        .arg("test#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        .arg(format!("test#{}", h_clone))
         .assert()
         .code(0);
 }
@@ -71,8 +84,8 @@ async fn gets_schema() {
     let matcher = move |request: &Request| {
         let body: Value = serde_json::from_str(str::from_utf8(&request.body).unwrap()).unwrap();
         let variables = &body["variables"];
-        assert!(variables["graphId"] == "test");
-        assert!(variables["variant"] == "test");
+        assert_eq!(variables["graphId"], "test");
+        assert_eq!(variables["variant"], "test");
         assert!(variables["hash"].is_null());
         true
     };
