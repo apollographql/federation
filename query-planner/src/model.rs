2cefe5beb7e79312f0054f02746213928220d4b1
Ran Magen <ran@apollographql.com>
2020-06-13 19:27:04 -0700

    Update `model.rs` to contained owned data required for executing a query plan.

--- query-planner/src/model.rs
+++ query-planner/src/model.rs
@@ -1,34 +1,67 @@
-use graphql_parser::query::SelectionSet;
+//! This is the model object for a QueryPlan trimmed down to only contain _owned_ fields
+//! that are required for executing a QueryPlan as implemented in the existing Apollo Gateway.
+//!
+//! The [SelectionSet] in the `requires` field of a [FetchNode] is trimmed to only be a list of
+//! either a [Field] or an [InlineFragment], since those are the only potential values needed to
+//! execute a query plan. Furthermore, within a [Field] or [InlineFragment], we only need
+//! names, aliases, type conditions, and recurively sub [SelectionSet]s.
 
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub struct QueryPlan(pub Option<PlanNode>);
+
+#[derive(Debug, PartialEq)]
+pub enum PlanNode {
+    Sequence(Vec<PlanNode>),
+    Parallel(Vec<PlanNode>),
+    Fetch(FetchNode),
+    Flatten {
+        path: Vec<ResponsePathElement>,
+        node: Box<PlanNode>,
+    },
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FetchNode {
+    pub service_name: String,
+    pub variable_usages: Vec<String>,
+    pub requires: Option<SelectionSet>,
+    pub operation: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Selection {
+    Field(Field),
+    InlineFragment(InlineFragment),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Field {
+    pub alias: Option<String>,
+    pub name: String,
+    pub selection_set: SelectionSet,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct InlineFragment {
+    pub type_condition: Option<String>,
+    pub selection_set: SelectionSet,
+}
+
+#[derive(Debug, PartialEq)]
 pub enum ResponsePathElement {
     Field(String),
     Idx(u32),
 }
 
-impl ToString for ResponsePathElement {
-    fn to_string(&self) -> String {
+impl fmt::Display for ResponsePathElement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            ResponsePathElement::Field(str) => str.to_string(),
-            ResponsePathElement::Idx(i) => i.to_string(),
+            ResponsePathElement::Field(str) => str.fmt(f),
+            ResponsePathElement::Idx(i) => i.fmt(f),
         }
     }
 }
 
-pub struct QueryPlan<'a>(pub Option<PlanNode<'a>>);
-
-pub struct FetchNode<'a> {
-    pub service_name: String,
-    pub variable_usages: Vec<String>,
-    pub requires: Option<SelectionSet<'a>>,
-    pub operation: String,
-}
-
-pub enum PlanNode<'a> {
-    Sequence(Vec<PlanNode<'a>>),
-    Parallel(Vec<PlanNode<'a>>),
-    Fetch(Box<FetchNode<'a>>),
-    Flatten {
-        path: Vec<ResponsePathElement>,
-        node: Box<PlanNode<'a>>,
-    },
-}
+pub type SelectionSet = Vec<Selection>;
