cf69443186bfb0bd11fc280294cce5b50a4a0ddb
Ran Magen <ran@apollographql.com>
2020-06-18 20:29:36 -0700

    make it compile #sigh

--- cli/src/telemetry/session.rs
+++ cli/src/telemetry/session.rs
@@ -125,7 +125,10 @@ impl Session {
         // keep the CLI waiting for 300 ms to send telemetry
         // if the request isn't sent in that time loose that report
         // to keep the experience fast for end users
-        println!("GITHUB_ACTIONS = {}", env::var("GITHUB_ACTIONS"));
+        debug!(
+            "GITHUB_ACTIONS = {}",
+            env::var("GITHUB_ACTIONS").unwrap_or("(not set)".to_string())
+        );
         let timeout = if self.platform.is_ci {
             debug!("setting timeout to 3000 millis");
             Duration::from_millis(3000)
