c144f1bdd211e05fb96348d80872420216a604d3
Ashi Krishnan <queerviolet@github.com>
2020-07-07 18:44:00 -0400

    Always use UNIX line endings in the snapshots.

--- graphql-parser/tests/helpers.rs
+++ graphql-parser/tests/helpers.rs
@@ -27,12 +27,12 @@ macro_rules! tests_for_parser {
                 let result = $parser(&input);
                 assert_snapshot!(
                     stringify!([<$name __ $parser __ win>]),
-                    format!("{}\n---\n{:#?}", &$input, &result));
+                    format!("{}\n---\n{:#?}", &$input.replace("\r\n", "\n"), &result));
                 if let Ok(ast) = result {
                     assert_eq!(ast.to_string(), expected);
                     assert_snapshot!(
                         stringify!([<$name __ visit _ $parser __ win>]),
-                        format!("{}\n---\n{:#?}", &$input, &[<visit _ $parser>](&ast)));
+                        format!("{}\n---\n{:#?}", &$input.replace("\r\n", "\n"), &[<visit _ $parser>](&ast)));
                 }
             }
         }
