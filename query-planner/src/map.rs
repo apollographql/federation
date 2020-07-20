be4b84a21a11deeb576e56e94f16de5a5e6c69a4
Ran Magen <ran@apollographql.com>
2020-07-20 15:11:05 -0700

    new QueryVisitor

--- query-planner/src/map.rs
+++ query-planner/src/map.rs
@@ -1,5 +1,5 @@
 use graphql_parser::query::*;
-use graphql_parser::{query, schema, Name, Map};
+use graphql_parser::{query, schema, Map, Name};
 use std::collections::HashMap;
 
 pub struct QueryVisitor<'q, 's> {
@@ -8,8 +8,40 @@ pub struct QueryVisitor<'q, 's> {
     pub fragments: HashMap<&'q str, &'q FragmentDefinition<'q>>,
 }
 
+impl<'q, 's: 'q> QueryVisitor<'q, 's> {
+    pub fn new(
+        schema: &'s schema::Document<'s>,
+        query: &'q query::Document<'q>,
+    ) -> QueryVisitor<'q, 's> {
+        let types: HashMap<&'s str, &'s schema::TypeDefinition<'s>> = schema
+            .definitions
+            .iter()
+            .flat_map(|d| match d {
+                schema::Definition::Type(td) => Some(td),
+                _ => None,
+            })
+            .map(|td| (td.name().unwrap(), td))
+            .collect();
+
+        let fragments: HashMap<&'q str, &FragmentDefinition<'q>> = query
+            .definitions
+            .iter()
+            .flat_map(|d| match d {
+                Definition::Fragment(frag) => Some((frag.name, frag)),
+                _ => None,
+            })
+            .collect();
+
+        QueryVisitor {
+            schema,
+            types,
+            fragments,
+        }
+    }
+}
+
 pub struct QueryPlanFrame<'s> {
-    pub parent_type_name: &'s str
+    pub parent_type_name: &'s str,
 }
 
 impl<'q, 's: 'q> Map for QueryVisitor<'q, 's> {
@@ -21,11 +53,19 @@ impl<'q, 's: 'q> query::Map<'q> for QueryVisitor<'q, 's> {
         unimplemented!()
     }
 
-    fn query_def<'a>(&'a mut self, def: &'q Definition<'q>, stack: &'a [Self::Output]) -> Self::Output {
+    fn query_def<'a>(
+        &'a mut self,
+        def: &'q Definition<'q>,
+        stack: &'a [Self::Output],
+    ) -> Self::Output {
         unimplemented!()
     }
 
-    fn sel_set<'a>(&'a mut self, sel_set: &'q SelectionSet<'q>, stack: &'a [Self::Output]) -> Self::Output {
+    fn sel_set<'a>(
+        &'a mut self,
+        sel_set: &'q SelectionSet<'q>,
+        stack: &'a [Self::Output],
+    ) -> Self::Output {
         unimplemented!()
     }
 
@@ -33,4 +73,3 @@ impl<'q, 's: 'q> query::Map<'q> for QueryVisitor<'q, 's> {
         unimplemented!()
     }
 }
-
