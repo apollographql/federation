06e1e9b9f803fc45e331f4f7f73c3dffcb08f356
Ran Magen <ran@apollographql.com>
2020-09-08 12:59:14 -0700

    Wrap the wasm query planner with `catch_unwind`

--- query-planner-wasm/src/lib.rs
+++ query-planner-wasm/src/lib.rs
@@ -2,6 +2,7 @@ extern crate wasm_bindgen;
 
 use apollo_query_planner::QueryPlanner;
 use js_sys::JsString;
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 static mut SCHEMA: Vec<String> = vec![];
@@ -23,12 +24,23 @@ pub fn get_query_planner(schema: JsString) -> usize {
 }
 
 #[wasm_bindgen(js_name = getQueryPlan)]
-pub fn get_query_plan(planner_ptr: usize, query: &str) -> JsValue {
+pub fn get_query_plan(planner_ptr: usize, query: &str) -> Vec<JsValue> {
     unsafe {
-        let planner = planner_ptr as *const QueryPlanner;
-        let planner: &QueryPlanner = &*planner;
-        let plan = planner.plan(query).unwrap();
-        JsValue::from_serde(&plan).unwrap()
+        let res = panic::catch_unwind(|| {
+            let planner = planner_ptr as *const QueryPlanner;
+            let planner: &QueryPlanner = &*planner;
+            let plan = planner.plan(query).unwrap();
+            JsValue::from_serde(&plan).unwrap()
+        });
+        dbg!(&res);
+
+        match res {
+            Ok(t) => vec![t, JsValue::UNDEFINED],
+            Err(e) => match e.downcast::<String>() {
+                Ok(s) => vec![JsValue::UNDEFINED, JsValue::from_str(&s)],
+                Err(_) => vec![JsValue::UNDEFINED, JsValue::from_str("Unknown error")],
+            },
+        }
     }
 }
 
@@ -37,6 +49,7 @@ mod tests {
     use crate::{get_query_plan, get_query_planner};
     use apollo_query_planner::model::{FetchNode, PlanNode, QueryPlan};
     use js_sys::JsString;
+    use wasm_bindgen::JsValue;
     use wasm_bindgen_test::*;
 
     #[wasm_bindgen_test]
@@ -54,8 +67,32 @@ mod tests {
             })),
         };
 
-        let result = get_query_plan(planner, query);
-        let plan = result.into_serde::<QueryPlan>().unwrap();
-        assert_eq!(plan, expected);
+        for _ in 0..5 {
+            let mut results = get_query_plan(planner, query);
+            assert_eq!(2, results.len());
+            assert_eq!(JsValue::UNDEFINED, results.pop().unwrap());
+            let actual_result = results.pop().unwrap();
+            let plan = actual_result.into_serde::<QueryPlan>().unwrap();
+            assert_eq!(plan, expected);
+        }
+    }
+
+    #[wasm_bindgen_test]
+    fn failing_to_plan_a_query_returns_an_error_string() {
+        let schema = include_str!("../../query-planner/tests/features/basic/csdl.graphql");
+        let planner = get_query_planner(JsString::from(schema));
+        let query = "query { me { thereIsNoSpoonField } }";
+
+        for _ in 0..5 {
+            let mut results = get_query_plan(planner, query);
+            dbg!(&results);
+            assert_eq!(2, results.len());
+            let error = results.pop().unwrap().as_string().unwrap();
+            assert_eq!(
+                "Cannot query field `thereIsNoSpoonField` on type `User`",
+                error
+            );
+            assert_eq!(JsValue::UNDEFINED, results.pop().unwrap());
+        }
     }
 }
