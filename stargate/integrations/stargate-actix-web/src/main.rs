b262844cb1679b72f3f3c2c3c5474fbd25c12335
Ran Magen <ran@apollographql.com>
2020-09-25 16:41:04 -0700

    add CORS and compression

--- stargate/integrations/stargate-actix-web/src/main.rs
+++ stargate/integrations/stargate-actix-web/src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer, Result};
+use actix_cors::Cors;
+use actix_web::{middleware, post, web, App, HttpResponse, HttpServer, Result};
 use apollo_stargate::common::Opt;
 use apollo_stargate::transports::http::{GraphQLRequest, RequestContext, ServerState};
 use apollo_stargate::Stargate;
@@ -30,8 +31,20 @@ async fn main() -> std::io::Result<()> {
     };
     let stargate = web::Data::new(ServerState { stargate });
 
-    HttpServer::new(move || App::new().data(stargate.clone()).service(index))
-        .bind(format!("127.0.0.1:{}", opt.address))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        let cors = Cors::new()
+            .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+            .allowed_origin("https://studio.apollographql.com")
+            .supports_credentials()
+            .finish();
+
+        App::new()
+            .wrap(middleware::Compress::default())
+            .wrap(cors)
+            .data(stargate.clone())
+            .service(index)
+    })
+    .bind(format!("127.0.0.1:{}", opt.address))?
+    .run()
+    .await
 }
