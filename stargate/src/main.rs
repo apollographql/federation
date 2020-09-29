e1b824f6ff41e308a90216f7f1bb428575c27bda
Ran Magen <ran@apollographql.com>
2020-09-29 12:51:12 -0700

    Move crates around so that the `stargate` crate is the binary, and its dependencies are in `crates`.

---
+++ stargate/src/main.rs
@@ -0,0 +1,54 @@
+use actix_cors::Cors;
+use actix_web::{middleware, post, web, App, HttpResponse, HttpServer, Result};
+use apollo_stargate_lib::common::Opt;
+use apollo_stargate_lib::transports::http::{GraphQLRequest, RequestContext, ServerState};
+use apollo_stargate_lib::Stargate;
+use env_logger::Env;
+use std::fs;
+
+#[post("/")]
+async fn index(
+    request: web::Json<GraphQLRequest>,
+    data: web::Data<ServerState<'static>>,
+) -> Result<HttpResponse> {
+    let ql_request = request.into_inner();
+    let context = RequestContext {
+        graphql_request: ql_request,
+    };
+    let result = match data.stargate.execute_query(&context).await {
+        Ok(result) => result,
+        Err(_) => todo!("handle error cases when executing query"),
+    };
+    Ok(HttpResponse::Ok().json(result))
+}
+
+static mut MANIFEST: String = String::new();
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    let opt = Opt::default();
+    let stargate = unsafe {
+        MANIFEST = fs::read_to_string(&opt.manifest)?;
+        Stargate::new(&MANIFEST)
+    };
+    let stargate = web::Data::new(ServerState { stargate });
+
+    HttpServer::new(move || {
+        let cors = Cors::new()
+            .allowed_methods(vec!["GET", "POST", "OPTIONS"])
+            .allowed_origin("https://studio.apollographql.com")
+            .supports_credentials()
+            .finish();
+
+        App::new()
+            .app_data(stargate.clone())
+            .wrap(middleware::Logger::default())
+            .wrap(middleware::Compress::default())
+            .wrap(cors)
+            .service(index)
+    })
+    .bind(format!("127.0.0.1:{}", opt.port))?
+    .run()
+    .await
+}
