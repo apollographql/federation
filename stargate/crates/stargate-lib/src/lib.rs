e4eab5428c8927a76b7f1d4f9857952c6c711715
Ran Magen <ran@apollographql.com>
2020-09-30 14:08:23 -0700

    Stargate suggestions to #135 + actix-web support (#186)
    This PR was developed on top of #135
    
    First and foremost, any changes to the existing _executor.rs_ and related code written in #135 are mostly there as **suggestions**. @jbaxleyiii lmk if you want to pair on these or feel free to take / reject parts. I didn't dig too deep into the implementation of the executor yet, I'm going to start doing that this week.
    
    Second: This PR separates out the Stargate library from the binary, for a few reasons:
    1. Separating out the business logic from the wrapping server makes for easier testing.
    2. The library doesn't choose an async runtime, nor does it assume any specific implementation of an HTTP server.
    3. Use actix-web instead of tide; we've discussed these options and feel more comfortable going with a more mature server implementation, as well as one that uses tokio.
    
    @jbaxleyiii If these changes look good, we can merge them into #135 and we can continue from there. If you want to do it differently, lmk!
    
    - [ ] Address `todo!` / `unimplemented!` / `FIXME` in the code
    - [ ] Tests for the executor (ideally these should be unit tests on the Stargate library, for coverage)
    - [ ] Integration tests. I was thinking we can set up acephei services locally and then the gateway would have `localhost` references to those on different ports, integration tests would be entirely localhost.
    - [ ] Add tracing/metrics middleware -- I believe there are some existing crates.
    - [ ] Write more log lines so that they're integrated with the logger of the http server (use `env_logger` ?)

---
+++ stargate/crates/stargate-lib/src/lib.rs
@@ -0,0 +1,71 @@
+use crate::request_pipeline::executor::execute_query_plan;
+use crate::request_pipeline::service_definition::ServiceDefinition;
+use crate::transports::http::{GraphQLResponse, RequestContext};
+use apollo_query_planner::helpers::directive_args_as_map;
+use apollo_query_planner::{QueryPlanner, QueryPlanningOptionsBuilder};
+use graphql_parser::schema;
+use std::collections::HashMap;
+
+pub mod common;
+mod request_pipeline;
+pub mod transports;
+mod utilities;
+
+#[derive(Clone)]
+pub struct Stargate<'app> {
+    service_list: HashMap<String, ServiceDefinition>,
+    pub planner: QueryPlanner<'app>,
+}
+
+impl<'app> Stargate<'app> {
+    pub fn new(schema: &'app str) -> Stargate<'app> {
+        // TODO(ran) FIXME: gql validation on schema
+        let planner = QueryPlanner::new(schema);
+        let service_list = get_service_list(&planner.schema);
+        Stargate {
+            planner,
+            service_list,
+        }
+    }
+
+    pub async fn execute_query(&self, request_context: &RequestContext) -> Result<GraphQLResponse> {
+        // TODO(ran) FIXME: gql validation on query
+        // TODO(james) actual request pipeline here
+        let options = QueryPlanningOptionsBuilder::default().build().unwrap();
+        let plan = self
+            .planner
+            .plan(&request_context.graphql_request.query, options)
+            .unwrap_or_else(|_| todo!("convert QueryPlanError to generic error"));
+
+        execute_query_plan(&plan, &self.service_list, &request_context).await
+    }
+}
+
+fn get_service_list(schema: &schema::Document) -> HashMap<String, ServiceDefinition> {
+    let schema_defintion: Option<&schema::SchemaDefinition> = schema
+        .definitions
+        .iter()
+        .filter_map(|d| match d {
+            schema::Definition::Schema(schema) => Some(schema),
+            _ => None,
+        })
+        .last();
+
+    if schema_defintion.is_none() {
+        todo!("handle error case")
+    }
+
+    apollo_query_planner::get_directive!(schema_defintion.unwrap().directives, "graph")
+        .map(|owner_dir| directive_args_as_map(&owner_dir.arguments))
+        .map(|args| {
+            (
+                String::from(args["name"]),
+                ServiceDefinition {
+                    url: String::from(args["url"]),
+                },
+            )
+        })
+        .collect()
+}
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
---
+++ stargate/crates/stargate-lib/src/utilities/deep_merge.rs
@@ -0,0 +1,95 @@
+use serde_json::Value;
+
+pub fn merge(target: &mut Value, source: &Value) {
+    if source.is_null() {
+        return;
+    }
+
+    match (target, source) {
+        (&mut Value::Object(ref mut map), &Value::Object(ref source)) => {
+            for (key, source_value) in source {
+                let target_value = map.entry(key.as_str()).or_insert_with(|| Value::Null);
+
+                if !target_value.is_null() && (source_value.is_object() || source_value.is_array())
+                {
+                    merge(target_value, source_value);
+                } else {
+                    *target_value = source_value.clone();
+                }
+            }
+        }
+        (&mut Value::Array(ref mut array), &Value::Array(ref source)) => {
+            for (index, source_value) in source.iter().enumerate() {
+                if let Some(target_value) = array.get_mut(index) {
+                    if !target_value.is_null() && source_value.is_object() {
+                        merge(target_value, source_value);
+                    } else {
+                        *target_value = source_value.clone();
+                    }
+                } else {
+                    array.push(source_value.clone());
+                }
+            }
+        }
+        (a, b) => {
+            *a = b.clone();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    // TODO(ran) FIXME: add test cases to address missing coverage lines (run grcov.sh)
+
+    #[test]
+    fn it_should_merge_objects() {
+        let mut first: Value = json!({"value1":"a","value2":"b"});
+        let second: Value = json!({"value1":"a","value2":"c","value3":"d"});
+
+        merge(&mut first, &second);
+
+        assert_eq!(
+            r#"{"value1":"a","value2":"c","value3":"d"}"#,
+            first.to_string()
+        );
+    }
+    #[test]
+    fn it_should_merge_objects_in_arrays() {
+        let mut first: Value = json!([{"value":"a","value2":"a+"},{"value":"b"}]);
+        let second: Value = json!([{"value":"b"},{"value":"c"}]);
+
+        merge(&mut first, &second);
+        assert_eq!(
+            r#"[{"value":"b","value2":"a+"},{"value":"c"}]"#,
+            first.to_string()
+        );
+    }
+    #[test]
+    fn it_should_merge_nested_objects() {
+        let mut first: Value = json!({"a":1,"b":{"someProperty":1,"overwrittenProperty":"clean"}});
+        let second: Value = json!({"b":{"overwrittenProperty":"dirty","newProperty":"new"},"c":4});
+
+        merge(&mut first, &second);
+
+        assert_eq!(
+            json!({"a":1,"b":{"someProperty":1,"overwrittenProperty":"dirty","newProperty":"new"},"c":4}),
+            first
+        );
+    }
+    #[test]
+    fn it_should_merge_nested_objects_in_arrays() {
+        let mut first: Value = json!({"a":1,"b":[{"c":1,"d":2}]});
+
+        let second: Value = json!({"e":2,"b":[{"f":3}]});
+
+        merge(&mut first, &second);
+
+        assert_eq!(
+            r#"{"a":1,"b":[{"c":1,"d":2,"f":3}],"e":2}"#,
+            first.to_string()
+        );
+    }
+}
