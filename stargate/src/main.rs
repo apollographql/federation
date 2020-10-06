84482954ca2d9d4b1ecf61f920021d810d60759e
Ran Magen <ran@apollographql.com>
2020-10-06 16:47:24 -0700

    Structured logging PoC

--- stargate/src/main.rs
+++ stargate/src/main.rs
@@ -4,13 +4,15 @@ use actix_web_opentelemetry::RequestTracing;
 use apollo_stargate_lib::common::Opt;
 use apollo_stargate_lib::transports::http::{GraphQLRequest, RequestContext, ServerState};
 use apollo_stargate_lib::Stargate;
-use env_logger::Env;
 use opentelemetry::api::{Key, Provider};
 use opentelemetry::sdk;
 use std::fs;
-use tracing::instrument;
+use tracing::{debug, instrument};
+use tracing_actix_web::TracingLogger;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::Registry;
+use tracing_subscriber::{EnvFilter, Registry};
 
 #[post("/")]
 #[instrument(skip(request, data))]
@@ -32,9 +34,10 @@ async fn index(
 static mut MANIFEST: String = String::new();
 
 fn init_observability() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    // env_logger::from_env(Env::default().default_filter_or("info")).init();
+    LogTracer::init().expect("Failed to set logger");
 
-    tracing::debug!("initializing jaeger trace exporter");
+    debug!("initializing jaeger trace exporter");
     let exporter = opentelemetry_jaeger::Exporter::builder()
         .with_collector_endpoint("http://localhost:14268/api/traces")
         .with_process(opentelemetry_jaeger::Process {
@@ -43,7 +46,7 @@ fn init_observability() -> Result<(), Box<dyn std::error::Error>> {
         })
         .init()?;
 
-    tracing::debug!("initializing jaeger trace provider");
+    debug!("initializing jaeger trace provider");
     let provider = sdk::Provider::builder()
         .with_simple_exporter(exporter)
         .with_config(sdk::Config {
@@ -52,14 +55,19 @@ fn init_observability() -> Result<(), Box<dyn std::error::Error>> {
         })
         .build();
 
-    {
-        let tracer = provider.get_tracer("stargate");
-        let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-        let subscriber = Registry::default().with(telemetry);
-        tracing::subscriber::set_global_default(subscriber).unwrap();
-    }
+    let subscriber = Registry::default()
+        .with(tracing_opentelemetry::layer().with_tracer(provider.get_tracer("stargate")))
+        .with(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")))
+        // .with(tracing_subscriber::fmt::layer())
+        .with(JsonStorageLayer)
+        .with(BunyanFormattingLayer::new(
+            String::from("stargate"),
+            std::io::stdout,
+        ));
 
-    tracing::debug!("setting global trace provider");
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+
+    debug!("setting global trace provider");
     opentelemetry::global::set_provider(provider);
 
     Ok(())
@@ -67,10 +75,10 @@ fn init_observability() -> Result<(), Box<dyn std::error::Error>> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    init_observability().expect("failed to initialize observability (traces, metrics)");
+    init_observability().expect("failed to initialize tracer.");
     let opt = Opt::default();
 
-    tracing::debug!("Initializing stargate instance");
+    debug!("Initializing stargate instance");
     let stargate = unsafe {
         MANIFEST = fs::read_to_string(&opt.manifest)?;
         Stargate::new(&MANIFEST)
@@ -86,7 +94,9 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .app_data(stargate.clone())
+            // XXX: Gives us access log looking messages, but should be redundant bc of `TracingLogger` ??
             .wrap(middleware::Logger::default())
+            .wrap(TracingLogger)
             .wrap(RequestTracing::new())
             .wrap(middleware::Compress::default())
             .wrap(cors)
