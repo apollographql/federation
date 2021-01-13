3b396bdeb5c6032394f0ec6f60fc5cb0616664aa
Ashi Krishnan <queerviolet@github.com>
2021-01-13 13:45:19 -0500

    Using crate

---
+++ using/src/constants.rs
@@ -0,0 +1,23 @@
+use lazy_static::lazy_static;
+
+use crate::{
+    version::Version,
+    request::Request,
+    spec::Spec,
+};
+
+pub const USING_SPEC_URL: &str = "https://specs.apollo.dev/using";
+pub const USING_VERSIONS: &[Version] = &[
+    Version(0, 1),
+];
+
+lazy_static! {
+    pub static ref USING_DEFAULT: Request = Request {
+        spec: Spec {
+            identity: USING_SPEC_URL.to_owned(),
+            default_prefix: "using".to_owned(),
+            version: USING_VERSIONS[0].clone(),
+        },
+        prefix: "using".to_owned()
+    };
+}
---
+++ using/src/lib.rs
@@ -0,0 +1,14 @@
+pub mod version;
+pub use version::Version;
+
+pub mod request;
+pub use request::Request;
+
+pub mod spec;
+pub use spec::Spec;
+
+pub mod constants;
+pub use constants::*;
+
+mod schema;
+pub use schema::*;
