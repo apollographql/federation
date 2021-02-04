894cfeded7b6121e6ad08ad42982b93f6c36b62d
Martijn Walraven <martijn@martijnwalraven.com>
2021-02-04 13:31:10 +0100

    Remove unnecessary declaration of `addSnapshotSerializer`

--- federation-integration-testsuite-js/src/snapshotSerializers/index.ts
+++ federation-integration-testsuite-js/src/snapshotSerializers/index.ts
@@ -3,14 +3,3 @@ export { default as selectionSetSerializer } from './selectionSetSerializer';
 export { default as typeSerializer } from './typeSerializer';
 export { default as graphqlErrorSerializer } from './graphqlErrorSerializer';
 export { default as queryPlanSerializer } from './queryPlanSerializer';
-
-declare global {
-  namespace jest {
-    interface Expect {
-      /**
-       * Adds a module to format application-specific data structures for serialization.
-       */
-      addSnapshotSerializer(serializer: import('pretty-format').Plugin): void;
-    }
-  }
-}
