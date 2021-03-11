c096f0fe3d8dfd512496c46206b1a3b807893579
Jesse Rosenberger <git@jro.cc>
2021-03-11 11:13:05 +0000

    format: adjust indentation to JS standard in rollup.config.js

--- harmonizer/rollup.config.js
+++ harmonizer/rollup.config.js
@@ -4,28 +4,28 @@ import nodePolyfills from 'rollup-plugin-node-polyfills';
 import path from 'path';
 
 export default [
-	{
-		input: path.resolve(__dirname, './js/index.mjs'),
-		output: {
-			name: 'composition',
-			file: path.resolve(__dirname, './dist/composition.js'),
-			format: 'iife',
-			globals: {
-				// This must be mocked in the runtime as an
-				// empty object. e.g., `node_fetch_1={}`.
-				// It will not be used for composition.
-				'node-fetch': 'node_fetch_1',
-			},
-			sourcemap: true,
-		},
-		// This just cuts off the awkward traversal that happens
-                // because of `apollo-env` which brings in all of
-                // `node-fetch` and thus, `http`, `https`, `stream`, etc.
-		external: ['node-fetch'],
-		plugins: [
-			nodePolyfills(),
-			resolve(),
-			commonjs(),
-		],
-	}
+  {
+    input: path.resolve(__dirname, './js/index.mjs'),
+    output: {
+      name: 'composition',
+      file: path.resolve(__dirname, './dist/composition.js'),
+      format: 'iife',
+      globals: {
+        // This must be mocked in the runtime as an
+        // empty object. e.g., `node_fetch_1={}`.
+        // It will not be used for composition.
+        'node-fetch': 'node_fetch_1',
+      },
+      sourcemap: true,
+    },
+    // This just cuts off the awkward traversal that happens because of
+    // `apollo-env` which brings in all of `node-fetch`, which in turn tries
+    // to load Node.js' `http`, `https`, `stream`, etc.
+    external: ['node-fetch'],
+    plugins: [
+      nodePolyfills(),
+      resolve(),
+      commonjs(),
+    ],
+  }
 ];
