0ef75de977c67be50b11111e99c77bcd49816008
Jesse Rosenberger <git@jro.cc>
2021-03-11 13:56:00 +0000

    Enable sane default inner attribute `warn`s and document implementations.
    Ref: https://github.com/apollographql/federation/pull/474#r588502988

--- harmonizer/src/lib.rs
+++ harmonizer/src/lib.rs
@@ -1,3 +1,33 @@
+/*!
+# Harmonizer
+
+This _harmonizer_ offers the ability to invoke a bundled version of the
+JavaScript library, [`@apollo/federation`], which _composes_ multiple sub-graphs
+into a super-graph.
+
+The bundled version of the federation library that is included is a JavaScript
+Immediately Invoked Function Expression ([IIFE]) that is created by running the
+[Rollup.js] bundler on the `@apollo/federation` package.
+
+When the [`harmonize`] function that this crate provides is called with a
+[`ServiceList`] (which is synonymous with the terminology and service list
+notion that exists within the JavaScript composition library), this crate uses
+[`deno_core`] to invoke the JavaScript within V8.  This is ultimately
+accomplished using [`rusty_v8`]'s V8 bindings to V8.
+
+While we intend for a future version of composition to be done natively within
+Rust, this allows us to provide a more stable transition using an already stable
+composition implementation while we work toward something else.
+
+[`@apollo/federation`]: https://npm.im/@apollo/federation
+[IIFE]: https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+[Rollup.js]: http://rollupjs.org/
+[`deno_core`]: https://crates.io/crates/deno_core
+[`rusty_v8`]: https://crates.io/crates/rusty_v8
+*/
+
+#![forbid(unsafe_code)]
+#![warn(missing_docs, future_incompatible, unreachable_pub, rust_2018_idioms)]
 use deno_core::Op;
 use deno_core::{json_op_sync, JsRuntime};
 use serde::{Deserialize, Serialize};
@@ -5,16 +35,31 @@ use std::sync::mpsc::channel;
 use std::{fmt::Display, io::Write};
 use thiserror::Error;
 
+/// The `ServiceDefinition` represents everything we need to know about a
+/// service (subgraph) for its GraphQL runtime responsibilities.  It is not
+/// at all different from the notion of [`ServiceDefinition` in TypeScript]
+/// used in Apollo Gateway's operation.
+///
+/// Since we'll be running this within a JavaScript environment these properties
+/// will be serialized into camelCase, to match the JavaScript expectations.
+///
+/// [`ServiceDefinition` in TypeScript]: https://github.com/apollographql/federation/blob/d2e34909/federation-js/src/composition/types.ts#L49-L53
 #[derive(Serialize)]
-// When serialized, we'll be putting this into JavaScript expecting camelCase.
 #[serde(rename_all = "camelCase")]
 pub struct ServiceDefinition {
+    /// The name of the service (subgraph).  We use this name internally to
+    /// in the representation of the composed schema and for designations
+    /// within the human-readable QueryPlan.
     pub name: String,
+    /// The routing/runtime URL where the subgraph can be found that will
+    /// be able to fulfill the requests it is responsible for.
     pub url: String,
+    /// The Schema Definition Language (SDL)
     pub type_defs: String,
 }
 
 impl ServiceDefinition {
+    /// Create a new [`ServiceDefinition`]
     pub fn new<N: Into<String>, U: Into<String>, D: Into<String>>(
         name: N,
         url: U,
@@ -28,11 +73,22 @@ impl ServiceDefinition {
     }
 }
 
+/// An ordered stack of the services (subgraphs) that, when composed in order
+/// by the composition algorithm, will represent the supergraph.
 pub type ServiceList = Vec<ServiceDefinition>;
 
+/// An error which occurred during JavaScript composition.
+///
+/// The shape of this error is meant to mimick that of the error created within
+/// JavaScript, which is a [`GraphQLError`] from the [`graphql-js`] library.
+///
+/// [`graphql-js']: https://npm.im/graphql
+/// [`GraphQLError`]: https://github.com/graphql/graphql-js/blob/3869211/src/error/GraphQLError.js#L18-L75
 #[derive(Debug, Error, Serialize, Deserialize, PartialEq)]
 pub struct CompositionError {
+    /// A human-readable description of the error that prevented composition.
     pub message: Option<String>,
+    /// [`CompositionErrorExtensions`]
     pub extensions: Option<CompositionErrorExtensions>,
 }
 
@@ -46,12 +102,37 @@ impl Display for CompositionError {
     }
 }
 
+/// Mimicking the JavaScript-world from which this error comes, this represents
+/// the `extensions` property of a JavaScript [`GraphQLError`] from the
+/// [`graphql-js`] library. Such errors are created when errors have prevented
+/// successful composition, which is accomplished using [`errorWithCode`]. An
+/// [example] of this can be seen within the `federation-js` JavaScript library.
+///
+/// [`graphql-js']: https://npm.im/graphql
+/// [`GraphQLError`]: https://github.com/graphql/graphql-js/blob/3869211/src/error/GraphQLError.js#L18-L75
+/// [`errorWithCode`]: https://github.com/apollographql/federation/blob/d7ca0bc2/federation-js/src/composition/utils.ts#L200-L216
+/// [example]: https://github.com/apollographql/federation/blob/d7ca0bc2/federation-js/src/composition/validate/postComposition/executableDirectivesInAllServices.ts#L47-L53
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct CompositionErrorExtensions {
+    /// An Apollo Federation composition error code.
+    ///
+    /// A non-exhaustive list of error codes that this includes, is:
+    ///
+    ///   - EXTERNAL_TYPE_MISMATCH
+    ///   - EXTERNAL_UNUSED
+    ///   - KEY_FIELDS_MISSING_ON_BASE
+    ///   - KEY_MISSING_ON_BASE
+    ///   - KEY_NOT_SPECIFIED
+    ///   - PROVIDES_FIELDS_MISSING_EXTERNAL
+    ///
+    /// ...and many more!  See the `federation-js` composition library for
+    /// more details (and search for `errorWithCode`).
     pub code: String,
 }
 
+/// An error that was received during composition within JavaScript.
 impl CompositionError {
+    /// Retrieve the error code from an error received during composition.
     pub fn code(&self) -> &str {
         if let Some(ref ext) = self.extensions {
             ext.code.as_str()
@@ -61,6 +142,9 @@ impl CompositionError {
     }
 }
 
+/// The `harmonize` function receives a [`ServiceList`] and invokes JavaScript
+/// composition on it.
+///
 pub fn harmonize(service_list: ServiceList) -> Result<String, Vec<CompositionError>> {
     // Initialize a runtime instance
     let mut runtime = JsRuntime::new(Default::default());
