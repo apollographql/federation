c2539be325cf7e68575c5bd82ab38a49c45ff78e
Jesse Rosenberger <git@jro.cc>
2021-03-13 12:37:05 +0000

    harmonizer: use explicit `include`, revert b123f5e and bump version = patch

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -1,15 +1,11 @@
-use std::path::Path;
+use std::fs::metadata;
 use std::process::Command;
-use std::{env, fs::metadata};
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("dist").join("composition.js");
-    if metadata(dest_path).is_err() {
+    if metadata("dist/composition.js").is_err() {
         assert!(Command::new("npm")
             .current_dir("../")
             .args(&["run", "compile:for-harmonizer-build-rs"])
-            .env("APOLLO_HARMONIZER_ROLLUP_BASE_DIR", out_dir)
             .status()
             .unwrap()
             .success());
