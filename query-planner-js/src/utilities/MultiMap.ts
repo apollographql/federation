247fb1022869b21b176fc4569c378fba19178202
Martijn Walraven <martijn@martijnwalraven.com>
2021-03-31 14:26:36 +0200

    Remove unnecessary assignment from `MultiMap`

--- query-planner-js/src/utilities/MultiMap.ts
+++ query-planner-js/src/utilities/MultiMap.ts
@@ -1,10 +1,10 @@
 export class MultiMap<K, V> extends Map<K, V[]> {
   add(key: K, value: V): this {
-    let values = this.get(key);
+    const values = this.get(key);
     if (values) {
       values.push(value);
     } else {
-      this.set(key, (values = [value]));
+      this.set(key, [value]);
     }
     return this;
   }
