bc65b991f15ff0015c980e765ab39ae6c2841460
bryncooke <bryncooke@gmail.com>
2021-06-09 18:46:22 +0200

    Open telemetry support This PR adds instrumentation to emit the following spans: * federation.request - total time to server a request. * federation.validate - time to validate the federated query. * federation.plan - time to plan the federated query. * federation.execute - the total time executing query. * federation.fetch - time fetching data from a subgraph. * federation.postprocessing - time to render the response from all the subgraph data.
    The instrumentation looks a little messy due to the requirement to set error status on spans on exception, but also if errors are added to the response.

---
+++ gateway-js/src/utilities/opentelemetry.ts
@@ -0,0 +1,8 @@
+export enum OpenTelemetrySpanNames {
+    REQUEST = "gateway.request",
+    PLAN = "gateway.plan",
+    FETCH = "gateway.fetch",
+    POST_PROCESSING = "gateway.postprocessing",
+    EXECUTE = "gateway.execute",
+    VALIDATE = "gateway.validate",
+}
