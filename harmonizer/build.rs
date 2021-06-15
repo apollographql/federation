e90992cba32f7fa3ebed595d8644c8e846708919
bryncooke <bryncooke@gmail.com>
2021-06-15 11:52:06 +0200

    Call npm install for cargo build if building bridge.js. Allows use as a git dependency in cargo.

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -5,6 +5,12 @@ fn main() {
     if metadata("dist/bridge.js").is_err() {
         assert!(Command::new("npm")
             .current_dir("../")
+            .args(&["install"])
+            .status()
+            .unwrap()
+            .success());
+        assert!(Command::new("npm")
+            .current_dir("../")
             .args(&["run", "compile:for-harmonizer-build-rs"])
             .status()
             .unwrap()
