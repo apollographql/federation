2c719cf7c643c720a501510d5962e8b22bf7664b
Avery Harnish <averyharnish@gmail.com>
2021-06-24 13:59:41 -0500

    chore: run npm install in build.rs

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -1,13 +1,22 @@
-use std::fs::metadata;
+use std::fs;
 use std::process::Command;
 
 fn main() {
-    if metadata("dist/composition.js").is_err() {
+    if fs::metadata("node_modules").is_err() {
+        assert!(Command::new("npm")
+            .current_dir("../")
+            .arg("install")
+            .status()
+            .expect("Could not execute `npm install`, is npm installed?")
+            .success())
+    }
+
+    if fs::metadata("dist/composition.js").is_err() {
         assert!(Command::new("npm")
             .current_dir("../")
             .args(&["run", "compile:for-harmonizer-build-rs"])
             .status()
-            .unwrap()
+            .expect("Could not compile harmonizer.")
             .success());
     }
 }
