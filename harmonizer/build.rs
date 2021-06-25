cea2039010a7ddd6c1868c7372493957c5608a08
Avery Harnish <avery@apollographql.com>
2021-06-25 17:01:05 -0500

    Revert "chore(harmonizer): run npm install in build.rs (#833)"
    This reverts commit b55a7b0e8234bf75b234f34e8a867d0818a201ab.

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -1,22 +1,13 @@
-use std::fs;
+use std::fs::metadata;
 use std::process::Command;
 
 fn main() {
-    if fs::metadata("node_modules").is_err() {
-        assert!(Command::new("npm")
-            .current_dir("../")
-            .arg("install")
-            .status()
-            .expect("Could not execute `npm install`, is npm installed?")
-            .success())
-    }
-
-    if fs::metadata("dist/composition.js").is_err() {
+    if metadata("dist/composition.js").is_err() {
         assert!(Command::new("npm")
             .current_dir("../")
             .args(&["run", "compile:for-harmonizer-build-rs"])
             .status()
-            .expect("Could not compile harmonizer.")
+            .unwrap()
             .success());
     }
 }
