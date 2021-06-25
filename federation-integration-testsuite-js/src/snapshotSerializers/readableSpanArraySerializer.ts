1418831bffb1eba0d71fbe38a1b31b655ac2bbc6
bryncooke <bryncooke@gmail.com>
2021-06-25 09:39:56 +0200

    Add OpenTelemetry support
    This PR adds instrumentation to emit the following spans:
    
    federation.request - total time to server a request.
    federation.validate - time to validate the federated query.
    federation.plan - time to plan the federated query.
    federation.execute - the total time executing query.
    federation.fetch - time fetching data from a subgraph.
    federation.postprocessing - time to render the response from all the subgraph data.
    The instrumentation looks a little messy due to the requirement to set error status on spans on exception, but also if errors are added to the response.
    
    It is a reissue of the following PRs
    https://github.com/apollographql/federation/pull/803
    https://github.com/apollographql/federation/pull/828

---
+++ federation-integration-testsuite-js/src/snapshotSerializers/readableSpanArraySerializer.ts
@@ -0,0 +1,39 @@
+import { Plugin } from 'pretty-format';
+import { ReadableSpan } from '@opentelemetry/tracing'
+
+export default {
+    test(value: any) {
+        return value && Array.isArray(value) && value.length > 0 && isReadableSpan(value[0]);
+    },
+
+    print(spans: ReadableSpan[]) {
+        // This method takes an array of spans and builds up a set of trees containing only the information we are interested in.
+        // Spans contain the ID of it's parent, unless it is a root span with no parent.
+        //
+        // The simplified data model is called a span mirror.
+        //
+        // The algorithm is:
+        // 1. create span mirrors for every span and place it in a map.
+        // For each span:
+        // 1. find the corresponding mirrors for itself and parent.
+        // 2. push the parent in to the parent's children array.
+
+        const root = {children:[]};
+        const spanMirrors = new Map<ReadableSpan | undefined, any>();
+        spanMirrors.set(undefined, root);
+        spans.forEach((s) => spanMirrors.set(s, {name: s.name, attributes:s.attributes, children: [], status: s.status}));
+        spans.forEach((s) => {
+            const spanMirror = spanMirrors.get(s);
+            const parentSpan = spans.find((s2) => s2.spanContext().spanId === s.parentSpanId);
+            const parentSpanMirror = spanMirrors.get(parentSpan);
+            parentSpanMirror.children.push(spanMirror);
+        });
+
+        return JSON.stringify(root.children, undefined, 2);
+    },
+} as Plugin;
+
+function isReadableSpan(arg: any): arg is ReadableSpan {
+    let isSpan = arg && 'kind' in arg && 'startTime' in arg && 'parentSpanId' in arg;
+    return isSpan;
+}
