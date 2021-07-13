89f7eda3ae1e4a5d1af967f4d567455c8d15934d
Trevor Scheer <trevor.scheer@gmail.com>
2021-07-13 09:29:02 -0700

    Revert to skipping missing types

--- federation-js/src/utilities/index.ts
+++ federation-js/src/utilities/index.ts
@@ -2,4 +2,3 @@ export { assert } from './assert';
 export { isNotNullOrUndefined } from './isNotNullOrUndefined';
 export { mapGetOrSet } from './mapGetOrSet';
 export { mapValues } from './mapValues';
-export { filterMap } from './filterMap';
