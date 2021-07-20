e981fc3499c2df2289bc7b1c023b7dad36bfc6fa
Trevor Scheer <trevor.scheer@gmail.com>
2021-07-20 12:44:03 -0700

    Cleanup matching logic

--- gateway-js/src/utilities/cleanErrorOfInaccessibleNames.ts
+++ gateway-js/src/utilities/cleanErrorOfInaccessibleNames.ts
@@ -4,66 +4,26 @@ export function cleanErrorOfInaccessibleNames(
   schema: GraphQLSchema,
   error: GraphQLError,
 ): GraphQLError {
+
+  const typeDotFieldRegex = /"([_A-Za-z][_0-9A-Za-z]*)\.([_A-Za-z][_0-9A-Za-z]*)"/g;
+  error.message = error.message.replace(typeDotFieldRegex, (match: string) => {
+    const [typeName, fieldName] = match.replace(/"/g, '',).split('.');
+    const type = schema.getType(typeName);
+    if (!type) {
+      return '[inaccessible field]';
+    } else {
+      const field = 'getFields' in type ? type.getFields()[fieldName] : null;
+      return field ? match : '[inaccessible field]';
+    }
+  });
+
   const typeRegex = /"([_A-Za-z][_0-9A-Za-z]*)"/g;
-  const typeDotFieldRegex =
-    /"([_A-Za-z][_0-9A-Za-z]*)\.([_A-Za-z][_0-9A-Za-z]*)"/g;
-
-  const schemaTypes = Object.keys(schema.getTypeMap());
-  let match: RegExpExecArray | null;
-  const typeDotFieldMatches: [string, string][] = [];
-  while ((match = typeDotFieldRegex.exec(error.message)) !== null) {
-    typeDotFieldMatches.push([match[1], match[2]]);
-  }
-
-  if (typeDotFieldMatches && typeDotFieldMatches.length > 0) {
-    const inaccessibleFields = typeDotFieldMatches.filter(
-      ([typeName, fieldName]) => {
-        const type = schema.getType(typeName);
-        if (!type) {
-          return true;
-        } else {
-          const field =
-            'getFields' in type ? type.getFields()[fieldName] : null;
-          return !field;
-        }
-      },
-    );
-    if (inaccessibleFields.length > 0) {
-      error.message = error.message.replace(
-        new RegExp(
-          inaccessibleFields
-            .map(([type, field]) => `("${type}.${field}")`)
-            .join('|'),
-          'g',
-        ),
-        '[inaccessible field]',
-      );
-    }
-  }
-
-  const typeMatches: string[] = [];
-  while ((match = typeRegex.exec(error.message)) !== null) {
-    if (match[1] !== 'resolveType' && match[1] !== 'isTypeOf') {
-      typeMatches.push(match[1]);
-    }
-  }
-  if (typeMatches && typeMatches.length > 0) {
-    const inaccessibleTypes = typeMatches.filter(
-      (match) => !schemaTypes.includes(match),
-    );
-
-    if (inaccessibleTypes.length > 0) {
-      error.message = error.message.replace(
-        new RegExp(
-          inaccessibleTypes
-            .map((namedType) => '("' + namedType + '")')
-            .join('|'),
-          'g',
-        ),
-        '[inaccessible type]',
-      );
-    }
-  }
+  error.message = error.message.replace(typeRegex, (match: string) => {
+    // Special cases in graphql-js that happen to match our regex.
+    if (match === '"isTypeOf"' || match === '"resolveType"') return match;
+    const typeName = match.replace(/"/g, '',);
+    return schema.getType(typeName) ? match : '[inaccessible type]';
+  })
 
   return error;
 }
