000318898b0bda66efb6388ad8b4e28a0f4e0e87
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-08-06 12:29:26 +0100

    Fix build process
    How was this even working?!

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -1,15 +1,16 @@
-use std::fs::metadata;
+use std::fs;
 use std::process::Command;
 
 fn main() {
-    if metadata("dist/bridge.js").is_err() {
-        assert!(Command::new("npm")
+    if fs::metadata("dist/bridge.js").is_err() {
+        let npm = which::which("npm").unwrap();
+        assert!(Command::new(&npm)
             .current_dir(fs::canonicalize("../").unwrap())
             .args(&["install"])
             .status()
             .unwrap()
             .success());
-        assert!(Command::new("npm")
+        assert!(Command::new(&npm)
             .current_dir(fs::canonicalize("../").unwrap())
             .args(&["run", "compile:for-harmonizer-build-rs"])
             .status()
