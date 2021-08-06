345078033c8a012c643503e0617aab32a0e91ca4
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-08-06 12:47:07 +0100

    Avoid fs::canonizalize()
    https://github.com/rust-lang/rust/issues/42869

--- harmonizer/build.rs
+++ harmonizer/build.rs
@@ -4,14 +4,17 @@ use std::process::Command;
 fn main() {
     if fs::metadata("dist/bridge.js").is_err() {
         let npm = which::which("npm").unwrap();
+        let current_dir = std::env::current_dir().unwrap();
+        let repo_dir = current_dir.parent().unwrap();
+
         assert!(Command::new(&npm)
-            .current_dir(fs::canonicalize("../").unwrap())
+            .current_dir(&repo_dir)
             .args(&["install"])
             .status()
             .unwrap()
             .success());
         assert!(Command::new(&npm)
-            .current_dir(fs::canonicalize("../").unwrap())
+            .current_dir(&repo_dir)
             .args(&["run", "compile:for-harmonizer-build-rs"])
             .status()
             .unwrap()
