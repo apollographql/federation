77d157023b78ebd097d7b6adfb2e8b47754e4346
Trevor Scheer <trevor.scheer@gmail.com>
2021-08-13 08:39:06 -0700

    Update v0.1 behavior with error throwing
    * Fix incorrectly written tests
    * Add requested test

--- gateway-js/src/core/__tests__/core.test.ts
+++ gateway-js/src/core/__tests__/core.test.ts
@@ -4,7 +4,59 @@ import { graphqlErrorSerializer } from 'apollo-federation-integration-testsuite'
 expect.addSnapshotSerializer(graphqlErrorSerializer);
 
 describe('core v0.1', () => {
-  it("doesn't throw errors / ignores 'for' args", () => {
+  it('throws no errors when using a valid @core v0.1 document', () => {
+    const supergraphSdl = `#graphql
+      schema
+        @core(feature: "https://specs.apollo.dev/core/v0.1")
+        @core(feature: "https://specs.apollo.dev/join/v0.1")
+        query: Query
+      }
+
+      directive @core(feature: String!) repeatable on SCHEMA
+
+      directive @join__field(
+        graph: join__Graph
+        requires: join__FieldSet
+        provides: join__FieldSet
+      ) on FIELD_DEFINITION
+
+      directive @join__type(
+        graph: join__Graph!
+        key: join__FieldSet
+      ) repeatable on OBJECT | INTERFACE
+
+      directive @join__owner(graph: join__Graph!) on OBJECT | INTERFACE
+
+      directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+
+      directive @tag(
+        name: String!
+      ) repeatable on FIELD_DEFINITION | INTERFACE | OBJECT | UNION
+
+      enum CacheControlScope {
+        PRIVATE
+        PUBLIC
+      }
+
+      scalar join__FieldSet
+
+      enum join__Graph {
+        WORLD @join__graph(name: "world", url: "https://world.api.com")
+      }
+
+      type Query {
+        hello: String! @join__field(graph: WORLD)
+      }
+    `;
+
+    const gateway = new ApolloGateway({
+      supergraphSdl,
+    });
+
+    expect(gateway.load()).resolves.toBeTruthy();
+  });
+
+  it('throws an error when the `for:` @core argument is used in a v0.1 document', () => {
     const supergraphSdl = `#graphql
       schema
         @core(feature: "https://specs.apollo.dev/core/v0.1")
@@ -57,11 +109,15 @@ describe('core v0.1', () => {
       supergraphSdl,
     });
 
-    expect(() => gateway.load()).resolves;
+    expect(() => gateway.load()).rejects.toMatchInlineSnapshot(`
+      Array [
+        "The \`for:\` argument is unsupported in @core v0.1 documents. Please upgrade to at least @core v0.2 (https://specs.apollo.dev/core/v0.2).",
+      ]
+    `);
   });
 });
 
-describe('supported features', () => {
+describe('core v0.2', () => {
   it("doesn't throw errors when using supported features", async () => {
     const supergraphSdl = `#graphql
       schema
@@ -128,7 +184,76 @@ describe('supported features', () => {
       supergraphSdl,
     });
 
-    expect(() => gateway.load()).resolves;
+    expect(gateway.load()).resolves.toBeTruthy();
+  });
+
+  it("doesn't throw errors when using unsupported features which have no `for:` argument", async () => {
+    const supergraphSdl = `#graphql
+      schema
+        @core(feature: "https://specs.apollo.dev/core/v0.2")
+        @core(feature: "https://specs.apollo.dev/tag/v0.1")
+        @core(feature: "https://specs.apollo.dev/unsupported-feature/v0.1") {
+        query: Query
+      }
+
+      directive @core(
+        feature: String!
+        as: String
+        for: core__Purpose
+      ) repeatable on SCHEMA
+
+      directive @join__field(
+        graph: join__Graph
+        requires: join__FieldSet
+        provides: join__FieldSet
+      ) on FIELD_DEFINITION
+
+      directive @join__type(
+        graph: join__Graph!
+        key: join__FieldSet
+      ) repeatable on OBJECT | INTERFACE
+
+      directive @join__owner(graph: join__Graph!) on OBJECT | INTERFACE
+
+      directive @join__graph(name: String!, url: String!) on ENUM_VALUE
+
+      directive @tag(
+        name: String!
+      ) repeatable on FIELD_DEFINITION | INTERFACE | OBJECT | UNION
+
+      enum CacheControlScope {
+        PRIVATE
+        PUBLIC
+      }
+
+      enum core__Purpose {
+        """
+        \`EXECUTION\` features provide metadata necessary to for operation execution.
+        """
+        EXECUTION
+
+        """
+        \`SECURITY\` features provide metadata necessary to securely resolve fields.
+        """
+        SECURITY
+      }
+
+      scalar join__FieldSet
+
+      enum join__Graph {
+        WORLD @join__graph(name: "world", url: "https://world.api.com")
+      }
+
+      type Query {
+        hello: String! @join__field(graph: WORLD)
+      }
+    `;
+
+    const gateway = new ApolloGateway({
+      supergraphSdl,
+    });
+
+    expect(gateway.load()).resolves.toBeTruthy();
   });
 
   it('throws errors when using unsupported features for EXECUTION', () => {
--- gateway-js/src/core/index.ts
+++ gateway-js/src/core/index.ts
@@ -15,10 +15,30 @@ export const ErrUnsupportedFeature = (feature: Feature) =>
     nodes: [feature.directive],
   });
 
+export const ErrForUnsupported = (feature: Feature) =>
+  err('ForUnsupported', {
+    message:
+      'The `for:` argument is unsupported in @core v0.1 documents. Please upgrade to at least @core v0.2 (https://specs.apollo.dev/core/v0.2).',
+    feature,
+    nodes: [feature.directive],
+  });
+
 export function featureSupport(this: CoreSchemaContext) {
-  // We support core v0.1. In the case that we're using it, "for" is not
-  // a concern and we can report no errors.
-  if (this.features.find("https://specs.apollo.dev/core/v0.1", true)) return;
+  const coreVersionZeroDotOne = this.features.find(
+    'https://specs.apollo.dev/core/v0.1',
+    true,
+  );
+  // Handle the case where the user is using core v0.1 and the "for" argument
+  // in a @core directive usage.
+  if (!!coreVersionZeroDotOne) {
+    for (const feature of this.features) {
+      if (feature.purpose) {
+        this.report(ErrForUnsupported(coreVersionZeroDotOne));
+        break;
+      }
+    }
+    return;
+  }
 
   for (const feature of this.features) {
     if (!feature.purpose) continue;
