9cb220135d776043acfe0cee411277a8f2ce0976
Jesse Rosenberger <git@jro.cc>
2021-10-20 12:43:04 +0000

    router-bridge: apply changes to use new `QueryPlanner` pattern
    This new router-bridge was introduced in
    https://github.com/apollographql/federation/pull/1090 but because it was
    based on another branch was still using the pattern which has not yet been
    employed within this branch.
    
    This should make the appropriate changes necessary to get TypeScript
    compilation working again and also apply the necessary changes to the Rust
    test error message assertions around operation validation (which have
    different wording, now).

--- router-bridge/js-src/plan.ts
+++ router-bridge/js-src/plan.ts
@@ -1,35 +1,21 @@
-import { ExecutionResult, parse, validate } from 'graphql';
-import {
-  QueryPlanner,
-  buildOperationContext,
-  BuildQueryPlanOptions,
-  buildComposedSchema,
-} from '@apollo/query-planner';
+import { ExecutionResult, parse } from 'graphql';
+import { QueryPlanner } from '@apollo/query-planner';
+
+import { buildSchema, operationFromDocument } from '@apollo/core';
 
 export function plan(
   schemaString: string,
-  queryString: string,
-  options: BuildQueryPlanOptions,
+  operationString: string,
   operationName?: string,
 ): ExecutionResult {
   try {
-    const schema = parse(schemaString);
-    const query = parse(queryString);
-    const composedSchema = buildComposedSchema(schema);
-
-    const validationErrors = validate(composedSchema, query);
-    if (validationErrors.length > 0) {
-      return { errors: validationErrors };
-    }
-
-    const operationContext = buildOperationContext(
-      composedSchema,
-      query,
-      operationName,
-    );
+    const composedSchema = buildSchema(schemaString);
+    const operationDocument = parse(operationString);
+    const operation =
+      operationFromDocument(composedSchema, operationDocument, operationName);
 
     const planner = new QueryPlanner(composedSchema);
-    return { data: planner.buildQueryPlan(operationContext, options) };
+    return { data: planner.buildQueryPlan(operation) };
   } catch (e) {
     return { errors: [e] };
   }
