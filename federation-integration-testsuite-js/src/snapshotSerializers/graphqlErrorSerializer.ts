3ff75af8bf4463d5736b83deeee8c9fd1b0af0d1
Trevor Scheer <trevor.scheer@gmail.com>
2021-11-11 17:29:31 -0800

    Update dependencies

--- federation-integration-testsuite-js/src/snapshotSerializers/graphqlErrorSerializer.ts
+++ federation-integration-testsuite-js/src/snapshotSerializers/graphqlErrorSerializer.ts
@@ -15,7 +15,7 @@ export default {
     }
     return print({
       message: value.message,
-      code: value.extensions ? value.extensions.code : 'MISSING_ERROR',
+      code: value.extensions?.code ?? 'MISSING_ERROR',
       locations: value.locations,
     });
   },
