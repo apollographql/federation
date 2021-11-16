5aaa6a4cdf3ce0cb2b08885c983ec669c39c5386
Trevor Scheer <trevor.scheer@gmail.com>
2021-11-16 10:58:40 -0800

    Add git add prompt to publishing prompt (#1213)

--- prompt-update-changelogs.js
+++ prompt-update-changelogs.js
@@ -1,10 +1,17 @@
-const Confirm = require('prompt-confirm');
-const prompt = new Confirm(
-  "Update and save CHANGELOGs (but don't commit them). When finished, type 'Y'"
-);
+const Confirm = require("prompt-confirm");
 
-return prompt.ask((answer) => {
+return (new Confirm(
+  "Update and save CHANGELOGs (but don't commit them). When finished, type 'Y'."
+).ask((answer) => {
   if (!answer) {
     process.exit(1);
+  } else {
+    new Confirm(
+      "Make sure you've `git add`ed your changes (but don't commit them). When finished, type 'Y'."
+    ).ask((answer) => {
+      if (!answer) {
+        process.exit(1);
+      }
+    });
   }
-});
+}));
