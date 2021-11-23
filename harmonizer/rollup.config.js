16ddd055f5cd0f9a4950724633c4b4838feb5e99
Trevor Scheer <trevor.scheer@gmail.com>
2021-11-23 13:26:51 -0800

    Specify option which has been defaulted to the opposite via major version

--- harmonizer/rollup.config.js
+++ harmonizer/rollup.config.js
@@ -11,15 +11,15 @@ export default [
       name: 'url_shim',
       file: path.resolve(__dirname, './dist/url_shim.js'),
       format: 'iife',
-      globals: {}
+      globals: {},
     },
     external: [],
     plugins: [
       json(),
       nodePolyfills(),
       resolve(),
-      commonjs()
-    ]
+      commonjs({ ignoreTryCatch: false }),
+    ],
   },
   {
     input: path.resolve(__dirname, './js/index.mjs'),
@@ -34,22 +34,19 @@ export default [
         'node-fetch': 'node_fetch_1',
 
         // v8 doesn't include the `URL` API natively, so we must shim it
-        'url': 'whatwg_url_1'
+        url: 'whatwg_url_1',
       },
       sourcemap: true,
     },
     // This just cuts off the awkward traversal that happens because of
     // `apollo-env` which brings in all of `node-fetch`, which in turn tries
     // to load Node.js' `http`, `https`, `stream`, etc.
-    external: [
-      'node-fetch',
-      'url'
-    ],
+    external: ['node-fetch', 'url'],
     plugins: [
       json(),
       nodePolyfills(),
       resolve(),
-      commonjs(),
+      commonjs({ ignoreTryCatch: false }),
     ],
-  }
+  },
 ];
