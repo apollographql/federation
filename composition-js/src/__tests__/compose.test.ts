apollographql/federation

commit b4e4fd471187b831d98391ed62a2345f80b7f388

Optimise indirect path computations (#1653)
This commit implements 2 optimizations for the computation of "indirect
paths" (think "following all possible key edges"):
1. it adds some pre-computation of edges-following-an-edge that skips
   filters some trivially unecessary options. Essentially, the
   precomputaton is relatively cheap and avoid repeatedly considering
   (and utlimately eliminating) those unecessary options.
2. query planning already has a mechanism by which "the computation of
   indirect paths folowing a given path" is memoized to save
   recomputation. This wasn't done for composition validation however
   and this commit correct that.

Those optimizations are particularly potent when composing a large number
of subgraphs with many entities spanning lots of subgraphs. On a fairly
large example (100+ subgraphs), this reduce composition time from 4+
"minutes" to less than 8 seconds ("on my machine").

query-graphs-js/src/graphPath.ts
@@ -387,7 +387,13 @@ export class GraphPath<TTrigger, RV extends Vertex = Vertex, TNullEdge extends n
    * The set of edges that may legally continue this path.
    */
   nextEdges(): readonly Edge[] {
-    return this.graph.outEdges(this.tail);
+    const tailEdge = this.edgeToTail;
+    // In theory, we could always return `this.graph.outEdges(this.tail)` here. But in practice, `nonTrivialFollowupEdges` may give us a subset
+    // of those "out edges" that avoids some of the edges that we know we don't need to check because they are guaranteed to be inefficient
+    // after the previous `tailEdge`. Note that is purely an optimization (see https://github.com/apollographql/federation/pull/1653 for more details).
+    return tailEdge
+      ? this.graph.nonTrivialFollowupEdges(tailEdge)
+      : this.graph.outEdges(this.tail);
   }
 
   /**
@@ -659,7 +665,7 @@ export class Unadvanceables {
   }
 }
 
-export function isUnadvanceable<V extends Vertex>(result: GraphPath<Transition, V>[] | Unadvanceables): result is Unadvanceables {
+export function isUnadvanceable(result: any[] | Unadvanceables): result is Unadvanceables {
   return result instanceof Unadvanceables;
 }
 
@@ -675,6 +681,65 @@ function createPathTransitionToEdgeFct(supergraph: Schema): (graph: QueryGraph, 
   }
 }
 
+/**
+ * Wraps a 'composition validation' path (one built from `Transition`) along with the information necessary to compute
+ * the indirect paths following that path, and cache the result of that computation when triggered.
+ *
+ * In other words, this is a `GraphPath<Transition, V>` plus lazy memoization of the computation of its following indirect
+ * options.
+ *
+ * The rational is that after we've reached a given path, we might never need to compute the indirect paths following it
+ * (maybe all the fields we'll care about are available "directive" (from the same subgraph)), or we might need to compute
+ * it once, or we might need them multiple times, but the way the algorithm work, we don't know this in advance. So
+ * this abstraction ensure that we only compute such indirect paths lazily, if we ever need them, but while ensuring
+ * we don't recompute them multiple times if we do need them multiple times.
+ */
+export class TransitionPathWithLazyIndirectPaths<V extends Vertex = Vertex> {
+  private lazilyComputedIndirectPaths: IndirectPaths<Transition, V> | undefined;
+
+  constructor(
+    readonly path: GraphPath<Transition, V>,
+    readonly conditionResolver: ConditionResolver,
+    readonly pathTransitionToEdge: (graph: QueryGraph, vertex: Vertex, transition: Transition) => Edge | null | undefined,
+  ) {
+  }
+
+  static initial<V extends Vertex = Vertex>(
+    supergraph: Schema,
+    initialPath: GraphPath<Transition, V>,
+    conditionResolver: ConditionResolver,
+  ): TransitionPathWithLazyIndirectPaths<V> {
+    return new TransitionPathWithLazyIndirectPaths(
+      initialPath,
+      conditionResolver,
+      createPathTransitionToEdgeFct(supergraph),
+    );
+  }
+
+  indirectOptions(): IndirectPaths<Transition, V> {
+    if (!this.lazilyComputedIndirectPaths) {
+      this.lazilyComputedIndirectPaths = this.computeIndirectPaths();
+    }
+    return this.lazilyComputedIndirectPaths;
+  }
+
+  private computeIndirectPaths(): IndirectPaths<Transition, V>  {
+    return advancePathWithNonCollectingAndTypePreservingTransitions(
+      this.path,
+      emptyContext,
+      this.conditionResolver,
+      [],
+      [],
+      t => t,
+      this.pathTransitionToEdge,
+    );
+  }
+
+  toString(): string {
+    return this.path.toString();
+  }
+}
+
 // Note: conditions resolver should return `null` if the condition cannot be satisfied. If it is satisfied, it has the choice of computing
 // the actual tree, which we need for query planning, or simply returning "undefined" which means "The condition can be satisfied but I didn't
 // bother computing a tree for it", which we use for simple validation.
@@ -684,11 +749,10 @@ function createPathTransitionToEdgeFct(supergraph: Schema): (graph: QueryGraph, 
 // meaning that as far as composition validation goes, we can ignore that transition (and anything that follows) and otherwise continue.
 export function advancePathWithTransition<V extends Vertex>(
   supergraph: Schema,
-  subgraphPath: GraphPath<Transition, V>,
+  subgraphPath: TransitionPathWithLazyIndirectPaths<V>,
   transition: Transition,
   targetType: NamedType,
-  conditionResolver: ConditionResolver,
-) : GraphPath<Transition, V>[] | Unadvanceables {
+) : TransitionPathWithLazyIndirectPaths<V>[] | Unadvanceables {
   // The `transition` comes from the supergraph. Now, it is possible that a transition can be expressed on the supergraph, but correspond
   // to an 'unsatisfiable' condition on the subgraph. Let's consider:
   // - Subgraph A:
@@ -747,7 +811,7 @@ export function advancePathWithTransition<V extends Vertex>(
     // If we consider a 'downcast' transition, it means that the target of that cast is composite, but also that the
     // last type of the subgraph path also is (that type is essentially the "source" of the cast).
     const supergraphRuntimeTypes = possibleRuntimeTypes(targetType as CompositeType);
-    const subgraphRuntimeTypes = subgraphPath.tailPossibleRuntimeTypes();
+    const subgraphRuntimeTypes = subgraphPath.path.tailPossibleRuntimeTypes();
     const intersection = supergraphRuntimeTypes.filter(t1 => subgraphRuntimeTypes.some(t2 => t1.name === t2.name)).map(t => t.name);
     // if we intersection is empty, it means whatever field got us here can never resolve into an object of the type we're casting
     // into. Essentially, we're good building a plan for this transition and whatever comes next: it'll just return nothing.
@@ -759,7 +823,7 @@ export function advancePathWithTransition<V extends Vertex>(
 
   debug.group(() => `Trying to advance ${subgraphPath} for ${transition}`);
   debug.group('Direct options:');
-  const directOptions = advancePathWithDirectTransition(supergraph, subgraphPath, transition, conditionResolver);
+  const directOptions = advancePathWithDirectTransition(supergraph, subgraphPath.path, transition, subgraphPath.conditionResolver);
   let options: GraphPath<Transition, V>[];
   const deadEnds: Unadvanceable[] = [];
   if (isUnadvanceable(directOptions)) {
@@ -772,28 +836,19 @@ export function advancePathWithTransition<V extends Vertex>(
     // no point in computing all the options.
     if (directOptions.length > 0 && isLeafType(targetType)) {
       debug.groupEnd(() => `reached leaf type ${targetType} so not trying indirect paths`);
-      return directOptions;
+      return createLazyTransitionOptions(directOptions, subgraphPath);
     }
     options = directOptions;
   }
   // Otherwise, let's try non-collecting edges and see if we can find some (more) options there.
   debug.group(`Computing indirect paths:`);
-  const pathTransitionToEdge = createPathTransitionToEdgeFct(supergraph);
-  const pathsWithNonCollecting = advancePathWithNonCollectingAndTypePreservingTransitions(
-    subgraphPath,
-    emptyContext,
-    conditionResolver,
-    [],
-    [],
-    t => t,
-    pathTransitionToEdge
-  );
+  const pathsWithNonCollecting = subgraphPath.indirectOptions();
   if (pathsWithNonCollecting.paths.length > 0) {
     debug.groupEnd(() => `${pathsWithNonCollecting.paths.length} indirect paths`);
     debug.group('Validating indirect options:');
     for (const nonCollectingPath of pathsWithNonCollecting.paths) {
       debug.group(() => `For indirect path ${nonCollectingPath}:`);
-      const pathsWithTransition = advancePathWithDirectTransition(supergraph, nonCollectingPath, transition, conditionResolver);
+      const pathsWithTransition = advancePathWithDirectTransition(supergraph, nonCollectingPath, transition, subgraphPath.conditionResolver);
       if (isUnadvanceable(pathsWithTransition)) {
         debug.groupEnd(() => `Cannot be advanced with ${transition}`);
         deadEnds.push(...pathsWithTransition.reasons);
@@ -808,7 +863,7 @@ export function advancePathWithTransition<V extends Vertex>(
   }
   debug.groupEnd(() => options.length > 0 ? advanceOptionsToString(options) : `Cannot advance ${transition} for this path`);
   if (options.length > 0) {
-    return options;
+    return createLazyTransitionOptions(options, subgraphPath);
   }
 
   const allDeadEnds = deadEnds.concat(pathsWithNonCollecting.deadEnds.reasons);
@@ -816,7 +871,7 @@ export function advancePathWithTransition<V extends Vertex>(
     const typeName = transition.definition.parent.name;
     const fieldName = transition.definition.name;
     const subgraphsWithDeadEnd = new Set(allDeadEnds.map(e => e.destSubgraph));
-    for (const [subgraph, schema] of subgraphPath.graph.sources.entries()) {
+    for (const [subgraph, schema] of subgraphPath.path.graph.sources.entries()) {
       if (subgraphsWithDeadEnd.has(subgraph)) {
         continue;
       }
@@ -832,7 +887,7 @@ export function advancePathWithTransition<V extends Vertex>(
           ? `type "${typeName}" has no @key defined in subgraph "${subgraph}"`
           : `none of the @key defined on type "${typeName}" in subgraph "${subgraph}" are resolvable (they are all declared with their "resolvable" argument set to false)`;
         allDeadEnds.push({
-          sourceSubgraph: subgraphPath.tail.source,
+          sourceSubgraph: subgraphPath.path.tail.source,
           destSubgraph: subgraph,
           reason: UnadvanceableReason.UNREACHABLE_TYPE,
           details: `cannot move to subgraph "${subgraph}", which has field "${transition.definition.coordinate}", because ${explanation}`
@@ -842,6 +897,17 @@ export function advancePathWithTransition<V extends Vertex>(
   }
 
   return new Unadvanceables(allDeadEnds);
+}
+
+function createLazyTransitionOptions<V extends Vertex>(
+  options: GraphPath<Transition, V>[],
+  origin: TransitionPathWithLazyIndirectPaths<V>,
+) : TransitionPathWithLazyIndirectPaths<V>[] {
+  return options.map(option => new TransitionPathWithLazyIndirectPaths(
+    option,
+    origin.conditionResolver,
+    origin.pathTransitionToEdge,
+  ));
 }
 
 // A set of excluded edges, that is a pair of a head vertex index and an edge index (since edge indexes are relative to their vertex).
@@ -929,7 +995,7 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
     const toAdvance = popMin(toTry);
     const nextEdges =  toAdvance.nextEdges().filter(e => !e.transition.collectOperationElements);
     if (nextEdges.length === 0) {
-      debug.log(`Nothing to try for ${toAdvance}: it has no non-collecting outbound edges`);
+      debug.log(() => `Nothing to try for ${toAdvance}: it has no non-collecting outbound edges`);
       continue;
     }
     debug.group(() => `From ${toAdvance}:`);
@@ -940,11 +1006,6 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
         continue;
       }
       excludedEdges = addEdgeExclusion(excludedEdges, edge);
-
-      if (isConditionExcluded(edge.conditions, excludedConditions)) {
-        debug.groupEnd(`Ignored: edge condition is excluded`);
-        continue;
-      }
 
       // We can only take a non-collecting transition that preserves the current type (typically,
       // jumping subgraphs through a key), with the exception of the federated graph roots, where
@@ -988,7 +1049,12 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
         // during composition validation where all costs are 0 to mean "we don't care about costs".
         // Meaning effectively that for validation, as soon as we have a path to a subgraph, we ignore
         // other options even if they may be "faster".
-        debug.groupEnd(`Ignored: a better path to the same subgraph already added`);
+        debug.groupEnd(() => `Ignored: a better (shorter) path to the same subgraph already added`);
+        continue;
+      }
+
+      if (isConditionExcluded(edge.conditions, excludedConditions)) {
+        debug.groupEnd(`Ignored: edge condition is excluded`);
         continue;
       }
 
@@ -1007,7 +1073,7 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
         // the same subgraph, we want to replace it by this one only if either 1) it is shorter or 2) if
         // it's of equal size, only if the condition cost are lower than the previous one.
         if (prevForSource && prevForSource[0].size === toAdvance.size + 1 && prevForSource[1] <= conditionResolution.cost) {
-          debug.groupEnd('Ignored: a better path to the same subgraph already added');
+          debug.groupEnd('Ignored: a better (less costly) path to the same subgraph already added');
           continue;
         }
 
@@ -1292,6 +1358,12 @@ export type SimultaneousPaths<V extends Vertex = Vertex> = OpGraphPath<V>[];
 
 type OpIndirectPaths<V extends Vertex> = IndirectPaths<OpTrigger, V, null, never>;
 
+/**
+ * Memoize the computation of indirect paths, like `TransitionPathWithLazyIndirectPaths` does, but for query planning.
+ *
+ * Here again, this is an optimization that avoids computing indirect paths eagerly (since we may not need them) but
+  * ensures we don't re-do their computation multiple times when we do need them multiple times.
+ */
 export class SimultaneousPathsWithLazyIndirectPaths<V extends Vertex = Vertex> {
   private lazilyComputedIndirectPaths: OpIndirectPaths<V>[];
 

composition-js/src/validate.ts
@@ -47,6 +47,8 @@ import {
   addConditionExclusion,
   SimultaneousPathsWithLazyIndirectPaths,
   advanceOptionsToString,
+  TransitionPathWithLazyIndirectPaths,
+  RootVertex,
 } from "@apollo/query-graphs";
 import { print } from "graphql";
 
@@ -223,12 +225,12 @@ export function computeSubgraphPaths(supergraphPath: RootPath<Transition>, subgr
   try {
     assert(!supergraphPath.hasAnyEdgeConditions(), () => `A supergraph path should not have edge condition paths (as supergraph edges should not have conditions): ${supergraphPath}`);
     const supergraphSchema = firstOf(supergraphPath.graph.sources.values())!;
-    const initialState = ValidationState.initial(supergraphPath.graph, supergraphPath.root.rootKind, subgraphs);
     const conditionResolver = new ConditionValidationResolver(supergraphSchema, subgraphs);
+    const initialState = ValidationState.initial({supergraph: supergraphPath.graph, supergraphSchema, kind: supergraphPath.root.rootKind, subgraphs, conditionResolver});
     let state = initialState;
     let isIncomplete = false;
     for (const [edge] of supergraphPath) {
-      const updated = state.validateTransition(supergraphSchema, edge, conditionResolver);
+      const updated = state.validateTransition(supergraphSchema, edge);
       if (!updated) {
         isIncomplete = true;
         break;
@@ -262,12 +264,27 @@ export class ValidationState {
     // Path in the supergraph corresponding to the current state.
     public readonly supergraphPath: RootPath<Transition>,
     // All the possible paths we could be in the subgraph.
-    public readonly subgraphPaths: RootPath<Transition>[]
+    public readonly subgraphPaths: TransitionPathWithLazyIndirectPaths<RootVertex>[]
   ) {
   }
 
-  static initial(supergraph: QueryGraph, kind: SchemaRootKind, subgraphs: QueryGraph) {
-    return new ValidationState(GraphPath.fromGraphRoot(supergraph, kind)!, initialSubgraphPaths(kind, subgraphs));
+  static initial({
+    supergraph,
+    supergraphSchema,
+    kind,
+    subgraphs,
+    conditionResolver,
+  }: {
+    supergraph: QueryGraph,
+    supergraphSchema: Schema,
+    kind: SchemaRootKind,
+    subgraphs: QueryGraph,
+    conditionResolver: ConditionValidationResolver,
+  }) {
+    return new ValidationState(
+      GraphPath.fromGraphRoot(supergraph, kind)!,
+      initialSubgraphPaths(kind, subgraphs).map((p) => TransitionPathWithLazyIndirectPaths.initial(supergraphSchema, p, conditionResolver.resolver)),
+    );
   }
 
   // Either return an error (we've found a path that cannot be validated), a new state (we've successfully handled the edge
@@ -277,7 +294,6 @@ export class ValidationState {
   validateTransition(
     supergraphSchema: Schema,
     supergraphEdge: Edge,
-    conditionResolver: ConditionValidationResolver
   ): ValidationState | undefined | ValidationError {
     assert(!supergraphEdge.conditions, () => `Supergraph edges should not have conditions (${supergraphEdge})`);
 
@@ -291,7 +307,6 @@ export class ValidationState {
         path,
         transition,
         targetType,
-        conditionResolver.resolver
       );
       if (isUnadvanceable(options)) {
         deadEnds.push(options);
@@ -306,7 +321,7 @@ export class ValidationState {
     }
     const newPath = this.supergraphPath.add(transition, supergraphEdge, noConditionsResolution);
     if (newSubgraphPaths.length === 0) {
-      return validationError(newPath, this.subgraphPaths, deadEnds);
+      return validationError(newPath, this.subgraphPaths.map((p) => p.path), deadEnds);
     }
     return new ValidationState(newPath, newSubgraphPaths);
   }
@@ -314,7 +329,7 @@ export class ValidationState {
   currentSubgraphs(): string[] {
     const subgraphs: string[] = [];
     for (const path of this.subgraphPaths) {
-      const source = path.tail.source;
+      const source = path.path.tail.source;
       if (!subgraphs.includes(source)) {
         subgraphs.push(source);
       }
@@ -347,7 +362,13 @@ class ValidationTraversal {
   constructor(supergraph: QueryGraph, subgraphs: QueryGraph) {
     this.supergraphSchema = firstOf(supergraph.sources.values())!;
     this.conditionResolver = new ConditionValidationResolver(this.supergraphSchema, subgraphs);
-    supergraph.rootKinds().forEach(k => this.stack.push(ValidationState.initial(supergraph, k, subgraphs)));
+    supergraph.rootKinds().forEach((kind) => this.stack.push(ValidationState.initial({
+      supergraph,
+      supergraphSchema: this.supergraphSchema,
+      kind,
+      subgraphs,
+      conditionResolver: this.conditionResolver
+    })));
     this.previousVisits = new QueryGraphState(supergraph);
   }
 
@@ -390,7 +411,7 @@ class ValidationTraversal {
       }
 
       debug.group(() => `Validating supergraph edge ${edge}`);
-      const newState = state.validateTransition(this.supergraphSchema, edge, this.conditionResolver);
+      const newState = state.validateTransition(this.supergraphSchema, edge);
       if (isValidationError(newState)) {
         debug.groupEnd(`Validation error!`);
         this.validationErrors.push(newState);

query-graphs-js/src/nonTrivialEdgePrecomputing.ts
@@ -1,0 +1,48 @@
+import { assert } from "@apollo/federation-internals";
+import { Edge, QueryGraph, QueryGraphState, simpleTraversal } from "./querygraph";
+
+export function preComputeNonTrivialFollowupEdges(graph: QueryGraph): (previousEdge: Edge) => readonly Edge[] {
+  const state = new QueryGraphState<undefined, readonly Edge[]>(graph);
+  simpleTraversal(graph, () => {}, (edge) => {
+    const followupEdges = graph.outEdges(edge.tail);
+    state.setEdgeState(edge, computeNonTrivialFollowups(edge, followupEdges));
+    return true;
+  });
+  return (previousEdge) => {
+    const nonTrivialFollowups = state.getEdgeState(previousEdge);
+    assert(nonTrivialFollowups, () => `Non-trivial followup edges of ${previousEdge} should have been computed`);
+    return nonTrivialFollowups;
+  }
+}
+
+function computeNonTrivialFollowups(edge: Edge, allFollowups: readonly Edge[]): readonly Edge[] {
+  switch (edge.transition.kind) {
+    case 'KeyResolution':
+      // After taking a key from subgraph A to B, there is no point of following that up with another key
+      // to subgraph C if that key has _the same_ conditions. This is because, due to the way key edges
+      // are created, if we have a key (with some conditions X) from B to C, then we are guaranteed to
+      // also have a key (with the same conditions X) from A to C, and so it's that later key we
+      // should be using in the first place. In other words, it's never better to do 2 hops rather than 1.
+      return allFollowups.filter((followup) => followup.transition.kind !== 'KeyResolution' || !sameConditions(edge, followup));
+    case 'RootTypeResolution':
+      // A 'RootTypeResolution' means that a query reached the query type (or another root type) in some
+      // subgraph A and we're looking at jumping to another subgraph B. But like for keys, there is
+      // no point in trying to jump directly to yet another subgpraph C from B, since we can always
+      // jump directly from A to C and it's better.
+      return allFollowups.filter((followup) => followup.transition.kind !== 'RootTypeResolution');
+    case 'SubgraphEnteringTransition':
+      // This is somewhat similar to 'RootTypeResolution' except that we're starting the query.
+      // But still, not doing "start of query" -> B -> C, since we can do "start of query" -> C
+      // and that's always better.
+      return allFollowups.filter((followup) => followup.transition.kind !== 'RootTypeResolution');
+    default:
+      return allFollowups;
+  }
+}
+
+function sameConditions(e1: Edge, e2: Edge): boolean {
+  if (!e1.conditions) {
+    return !e2.conditions;
+  }
+  return !!e2.conditions && e1.conditions.equals(e2.conditions);
+}

commit fc5cc4c24a658cf092a49cf4bb5e0fe4abada720

Fix validation issue introduced by #1653
The "edge following other edges"-precomputation optimization introduced
in #1653 introduced a regression in the composition composition code.

The problem is due to the fact that the optimization was assuming that
the validation/query planning algorithm would always find optimal paths,
but the algorithm was a bit too aggressive at avoid some edges given
that new optimization is in place.

Let's use the example this patch uses as test to illustrate:
```graphql
type Query {
  t: T
}

type T @key(fields: "k1") {
  k1: Int
}
```
```graphql
type T @key(fields: "k2") @key(fields: "k1") {
  k1: Int
  k2: Int
}
```
```graphql
type T @key(fields: "k2") {
  k2: Int
  v: Int
}
```
When the algorithm is on `T` in `A` and looks for `v`, it tries to find
where keys can lead it. In doing so, it:
1. looked first at key `k2` to B and checked if it was usable. And it is,
   provided we first use key `k1` to get `k1` from B. It's inefficient
   but possiblly (it's arguably dumb, but it's an algorithm, it has no
   shame).
2. then looked at key `k1` to B. Now, it actually ignored that edge
   because it already had a path to B (the one from the previous point).
   That's because the validation algorithm doesn't care one bit about
   efficiency, and that is not the issue. The issue is that the
   algorithm was also marking that edge "excluded" for remainder of
   the "current loop".
3. it looked at `k2` to C, which is actually edge we want to use. But
   unfortunately, to use it, we first need to get `k2` from B using `k1`
   and as mentioned in the previous point, we had mistankenly excluded
   that edge, so that edge was considered a dead end when it shouldn't.
4. at that point the algorithm was actually looking back at the path
   it found in point 1, which got it to B, and checked if it could get
   to C from there. That's where the algorithm "used to" make progress
   by, at this point, using the `k2` edge from B to C. But the

query-graphs-js/src/graphPath.ts
@@ -1008,7 +1008,6 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
         debug.groupEnd(`Ignored: edge is excluded`);
         continue;
       }
-      excludedEdges = addEdgeExclusion(excludedEdges, edge);
 
       // We can only take a non-collecting transition that preserves the current type (typically,
       // jumping subgraphs through a key), with the exception of the federated graph roots, where
@@ -1067,7 +1066,7 @@ function advancePathWithNonCollectingAndTypePreservingTransitions<TTrigger, V ex
         edge,
         conditionResolver,
         context,
-        excludedEdges,
+        addEdgeExclusion(excludedEdges, edge),
         excludedConditions,
       );
       if (conditionResolution.satisfied) {

composition-js/src/__tests__/compose.test.ts
@@ -1728,51 +1728,96 @@ describe('composition', () => {
     `);
   });
 
-  // We have specific validation tests in `validation_errors.test.ts` but this one test
-  // just check the associated error code is correct (since we check most composition error
-  // codes in this file)
-  it('use the proper error code for composition validation errors', () => {
-    const subgraphA = {
-      typeDefs: gql`
-        type Query {
-          a: A
-        }
+  describe('satisfiablility validation', () => {
+    // We have specific validation tests for validation errors in `validation_errors.test.ts` but this one
+    // test just check the associated error code is correct (since we check most composition error codes
+    // in this file)
+    it('uses the proper error code', () => {
+      const subgraphA = {
+        typeDefs: gql`
+          type Query {
+            a: A
+          }
 
-        type A @shareable {
-          x: Int
-        }
-      `,
-      name: 'subgraphA',
-    };
+          type A @shareable {
+            x: Int
+          }
+        `,
+        name: 'subgraphA',
+      };
 
-    const subgraphB = {
-      typeDefs: gql`
-        type A @shareable {
-          x: Int
-          y: Int
-        }
-      `,
-      name: 'subgraphB',
-    };
+      const subgraphB = {
+        typeDefs: gql`
+          type A @shareable {
+            x: Int
+            y: Int
+          }
+        `,
+        name: 'subgraphB',
+      };
 
-    const result = composeAsFed2Subgraphs([subgraphA, subgraphB]);
+      const result = composeAsFed2Subgraphs([subgraphA, subgraphB]);
 
-    expect(result.errors).toBeDefined();
-    expect(errors(result).map(([code]) => code)).toStrictEqual(['SATISFIABILITY_ERROR']);
-    expect(errors(result).map(([_, msg]) => msg)).toMatchStringArray([
-      `
-      The following supergraph API query:
-      {
-        a {
-          y
+      expect(result.errors).toBeDefined();
+      expect(errors(result).map(([code]) => code)).toStrictEqual(['SATISFIABILITY_ERROR']);
+      expect(errors(result).map(([_, msg]) => msg)).toMatchStringArray([
+        `
+        The following supergraph API query:
+        {
+          a {
+            y
+          }
         }
-      }
-      cannot be satisfied by the subgraphs because:
-      - from subgraph "subgraphA":
-        - cannot find field "A.y".
-        - cannot move to subgraph "subgraphB", which has field "A.y", because type "A" has no @key defined in subgraph "subgraphB".
-      `],
-    );
+        cannot be satisfied by the subgraphs because:
+        - from subgraph "subgraphA":
+          - cannot find field "A.y".
+          - cannot move to subgraph "subgraphB", which has field "A.y", because type "A" has no @key defined in subgraph "subgraphB".
+        `],
+      );
+    });
+
+    it('handles indirectly reacheable keys', () => {
+      // This tests ensure that a regression introduced by https://github.com/apollographql/federation/pull/1653
+      // is properly fixed. All we want to check is that validation succeed on this example, which it should.
+
+      const subgraphA = {
+        typeDefs: gql`
+          type Query {
+            t: T
+          }
+
+          type T @key(fields: "k1") {
+            k1: Int
+          }
+        `,
+        name: 'subgraphA',
+      };
+
+      const subgraphB = {
+        typeDefs: gql`
+          # Note: the ordering of the key happens to matter for this to be a proper reproduction of the
+          # issue #1653 created.
+          type T @key(fields: "k2") @key(fields: "k1") {
+            k1: Int
+            k2: Int
+          }
+        `,
+        name: 'subgraphB',
+      };
+
+      const subgraphC = {
+        typeDefs: gql`
+          type T @key(fields: "k2") {
+            k2: Int
+            v: Int
+          }
+        `,
+        name: 'subgraphC',
+      };
+
+      const result = composeAsFed2Subgraphs([subgraphA, subgraphB, subgraphC]);
+      assertCompositionSuccess(result);
+    });
   });
 
   describe('field sharing', () => {