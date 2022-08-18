7735a881350a1c54e7eb38982dc9d53db8136eae
Shane Myrick <shane@apollographql.com>
2022-08-18 12:56:25 -0700

    Add unit test

--- gateway-js/src/schema-helper/__tests__/addExtensions.test.ts
+++ gateway-js/src/schema-helper/__tests__/addExtensions.test.ts
@@ -3,9 +3,34 @@ import { addExtensions } from '../addExtensions';
 const { version } = require('../../../package.json');
 
 describe('addExtensions', () => {
+
   it('adds gateway extensions to a schema', async () => {
     const schema = buildSchema('type Query { hello: String }');
     expect(schema.extensions).toEqual({});
     expect(addExtensions(schema).extensions).toEqual({ apollo: { gateway: { version: version } } });
+  });
+
+  it('does not delete existing extensions', async () => {
+    const schema = buildSchema('type Query { hello: String }');
+    expect(schema.extensions).toEqual({});
+    schema.extensions = {
+      one: 1,
+      apollo: {
+        two: 2,
+        gateway: {
+          three: 3
+        }
+      }
+    };
+    expect(addExtensions(schema).extensions).toEqual({
+      one: 1,
+      apollo: {
+        two: 2,
+        gateway: {
+          three: 3,
+          version: version
+        }
+      }
+    });
   });
 });
