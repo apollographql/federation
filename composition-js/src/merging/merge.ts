32c5c5bbbe2332fcb6b0511235e859ac78af4ebc
Chris Lenfest <clenfest@apollographql.com>
2022-08-24 18:40:08 -0500

    simplify code

--- composition-js/src/merging/merge.ts
+++ composition-js/src/merging/merge.ts
@@ -1867,8 +1867,7 @@ class Merger {
 
     // if there is exactly one subgraph with a description for this directive, or if they all match, set the destination description
     const descriptions = sources
-      .filter(isDefined)
-      .map(src => src.description)
+      .map(src => src?.description)
       .filter(isDefined);
 
     if (descriptions.length > 0 && descriptions.every(d => descriptions[0] === d)) {
