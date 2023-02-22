25999d0a1aa5197f688b24f5349185c87596a954
Trevor Scheer <trevor.scheer@gmail.com>
2023-02-21 17:00:57 -0800

    finalize changes

--- scripts/update-next-tags.mjs
+++ scripts/update-next-tags.mjs
@@ -1,33 +1,52 @@
-import { exec } from 'child_process';
-import fetch from 'node-fetch';
-import semver from 'semver';
+// @ts-check
+import { exec } from "child_process";
+import { readFileSync } from "fs";
+import fetch from "node-fetch";
+import { resolve } from "path";
+import semver from "semver";
 
-const pkg = '@apollo/gateway';
-const response = await fetch(`https://registry.npmjs.org/${encodeURIComponent(pkg)}`);
-const json = await response.json();
+// Collect all the packages that we publish
+const workspaces = JSON.parse(
+  readFileSync(resolve("package.json"), "utf-8")
+).workspaces;
+const publishedPackages = workspaces
+  .map((workspace) => {
+    const packageFile = JSON.parse(
+      readFileSync(resolve(workspace, "package.json"), "utf-8")
+    );
+    if (packageFile.private === true) return null;
+    return packageFile.name;
+  })
+  .filter(Boolean);
 
-const distTags = json['dist-tags'];
-const sortedVersions = semver.sort(Object.keys(json.versions));
-const mostRecentVersion = sortedVersions.pop();
-const testVersion = distTags['test'];
+for (const pkg in publishedPackages) {
+  const response = await fetch(
+    `https://registry.npmjs.org/${encodeURIComponent(pkg)}`
+  );
+  const json = await response.json();
+  const nextVersion = json["dist-tags"].next;
+  const sortedVersions = semver.sort(Object.keys(json.versions));
+  const mostRecentVersion = sortedVersions.pop();
 
-console.log(`Found most recent version of ${pkg}: ${mostRecentVersion}`);
-console.log(`Current \`test\` tag version of ${pkg}: ${testVersion}`);
+  console.log(`Found most recent version of ${pkg}: ${mostRecentVersion}`);
+  console.log(`Current \`next\` tag version of ${pkg}: ${nextVersion}`);
 
-if (testVersion !== mostRecentVersion) {
-  console.log(`\`test\` tag is behind, updating...`);
-  exec(`npm dist-tag add ${pkg}@${mostRecentVersion} test`, (e) => {
-    if (e) {
-
-      console.error(e);
-      console.error('Error occurred while updating `test` tag!');
-      process.exit(1);
-    } else {
-      console.log('`next` tag updated successfully!');
-      process.exit(0);
-    }
-  });
-} else {
-  console.log('No action needed, `test` tag is pointed to most recent version');
-  process.exit(0);
+  if (nextVersion !== mostRecentVersion) {
+    console.log(`\`next\` tag is behind, updating...`);
+    exec(`npm dist-tag add ${pkg}@${mostRecentVersion} next`, (e) => {
+      if (e) {
+        console.error(e);
+        console.error("Error occurred while updating `next` tag!");
+        process.exit(1);
+      } else {
+        console.log("`next` tag updated successfully!");
+        process.exit(0);
+      }
+    });
+  } else {
+    console.log(
+      "No action needed, `next` tag is pointed to most recent version"
+    );
+    process.exit(0);
+  }
 }
