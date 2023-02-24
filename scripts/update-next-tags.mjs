a4ab390418b84256519986afa655f0d5426acf69
Trevor Scheer <trevor.scheer@gmail.com>
2023-02-24 09:11:44 -0800

    Keep `next` tag up-to-date on publish (#2416)
    This script runs whenever the release action runs. It takes the following steps:
    
    * gather package names from the repository (only the ones that we publish)
       for each package
    * query npm for the most recent (semver greatest) version as well as the version of the next dist tag
       compare the two
    * if next is behind, update the next tag to the semver greatest version

---
+++ scripts/update-next-tags.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+import { exec } from "child_process";
+import { readFileSync } from "fs";
+import fetch from "node-fetch";
+import { resolve } from "path";
+import semver from "semver";
+
+// Collect all the packages that we publish
+const workspaces = JSON.parse(
+  readFileSync(resolve("package.json"), "utf-8")
+).workspaces;
+const publishedPackages = workspaces
+  .map((workspace) => {
+    const packageFile = JSON.parse(
+      readFileSync(resolve(workspace, "package.json"), "utf-8")
+    );
+    if (packageFile.private === true) return null;
+    return packageFile.name;
+  })
+  .filter(Boolean);
+
+for (const pkg in publishedPackages) {
+  const response = await fetch(
+    `https://registry.npmjs.org/${encodeURIComponent(pkg)}`
+  );
+  const json = await response.json();
+  const nextVersion = json["dist-tags"].next;
+  const sortedVersions = semver.sort(Object.keys(json.versions));
+  const mostRecentVersion = sortedVersions.pop();
+
+  console.log(`Found most recent version of ${pkg}: ${mostRecentVersion}`);
+  console.log(`Current \`next\` tag version of ${pkg}: ${nextVersion}`);
+
+  if (nextVersion !== mostRecentVersion) {
+    console.log(`\`next\` tag is behind, updating...`);
+    exec(`npm dist-tag add ${pkg}@${mostRecentVersion} next`, (e) => {
+      if (e) {
+        console.error(e);
+        console.error("Error occurred while updating `next` tag!");
+        process.exit(1);
+      } else {
+        console.log("`next` tag updated successfully!");
+        process.exit(0);
+      }
+    });
+  } else {
+    console.log(
+      "No action needed, `next` tag is pointed to most recent version"
+    );
+    process.exit(0);
+  }
+}
