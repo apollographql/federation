9e0653d2bf9194b9b72e00b1a54022b7f3d52895
Sylvain Lebresne <sylvain@apollographql.com>
2023-03-07 11:40:48 +0100

    Adds debug query planner option to skip query planning for a single subgraph
    This commit adds a new option to the query planner config that, when
    used and when the supergraph is built from only a single subgraph, skip
    the query planning algorithm and instead generates a single fetch (to
    the one subgraph) that directly uses the gateway "input" query.
    
    This option is disabled by default and marked as a "debug" option as it
    is a bit of a hack and so we're not suggesting committing to supporting
    it forever. But it is easy enough to have now and can be convenient at
    least for testing/debugging.
    
    Note in particular that, when used, some of the features enabled by
    the query planner (for instance `@defer`) cannot be enabled. The option
    will also stop having any effect as soon as another subgraph is added,
    not matter what that 2nd subgraph contains.

---
+++ gateway-js/src/__tests__/gateway/testUtils.ts
@@ -0,0 +1,73 @@
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { ApolloServer } from 'apollo-server';
+import { GraphQLSchemaModule } from '@apollo/subgraph/src/schema-helper';
+import { ApolloServerPluginInlineTrace } from 'apollo-server-core';
+import { ApolloGateway, GatewayConfig } from '../..';
+import { ServiceDefinition } from '@apollo/federation-internals';
+import { ApolloServerExpressConfig, } from 'apollo-server-express';
+import fetch, { Response } from 'node-fetch';
+
+export class Services {
+  constructor(
+    readonly subgraphServers: ApolloServer[],
+    readonly gateway: ApolloGateway,
+    readonly gatewayServer: ApolloServer,
+    readonly gatewayUrl: string,
+  ) {
+  }
+
+  async queryGateway(query: string): Promise<Response> {
+    return fetch(this.gatewayUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+  }
+
+  async stop() {
+    for (const server of this.subgraphServers) {
+      await server.stop();
+    }
+    await this.gatewayServer.stop();
+  }
+}
+
+async function startFederatedServer(modules: GraphQLSchemaModule[]) {
+  const schema = buildSubgraphSchema(modules);
+  const server = new ApolloServer({
+    schema,
+    // Manually installing the inline trace plugin means it doesn't log a message.
+    plugins: [ApolloServerPluginInlineTrace()],
+  });
+  const { url } = await server.listen({ port: 0 });
+  return { url, server };
+}
+
+export async function startSubgraphsAndGateway(
+  servicesDefs: ServiceDefinition[],
+  config?: {
+    gatewayConfig?: GatewayConfig,
+    gatewayServerConfig?: ApolloServerExpressConfig,
+  }
+): Promise<Services> {
+  const backendServers = [];
+  const serviceList = [];
+  for (const serviceDef of servicesDefs) {
+    const { server, url } = await startFederatedServer([serviceDef]);
+    backendServers.push(server);
+    serviceList.push({ name: serviceDef.name, url });
+  }
+
+  const gateway = new ApolloGateway({ 
+    serviceList,
+    ...config?.gatewayConfig,
+  });
+  const gatewayServer = new ApolloServer({
+    gateway,
+    ...config?.gatewayServerConfig,
+  });
+  const { url: gatewayUrl } = await gatewayServer.listen({ port: 0 });
+  return new Services(backendServers, gateway, gatewayServer, gatewayUrl);
+}
