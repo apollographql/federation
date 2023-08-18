4db499b0c12204a500fc7965a6c6055f87a4bef6
Noemi <45180344+unflxw@users.noreply.github.com>
2023-08-19 01:58:44 +0200

    Gateway OpenTelemetry improvements (#2700)
    Add more information to OpenTelemetry spans.
    
    Rename `operationName` to `graphql.operation.name` and add a
    `graphql.operation.type` attribute, in conformance with the OpenTelemetry
    Semantic Conventions for GraphQL. The `operationName` attribute is now
    deprecated, but it is still emitted alongside `graphql.operation.name`.
    
    Add a `graphql.document` span attribute to the `gateway.request` span,
    containing the entire GraphQL source sent in the request. This feature
    is disabled by default.
    
    When one or more GraphQL or internal errors occur, report them in the
    OpenTelemetry span in which they took place, as an exception event. This
    feature is disabled by default.
    
    To enable the `graphql.document` span attribute and the exception event
    reporting, add the following entries to your `ApolloGateway` instance
    configuration:
    
    ```ts
    const gateway = new ApolloGateway({
      // ...
      telemetry: {
        // Set to `true` to include the `graphql.document` attribute
        includeDocument: true,
        // Set to `true` to report all exception events, or set to a number
        // to report at most that number of exception events per span
        reportExceptions: true,
        // or: reportExceptions: 1
      },
    });
    ```

--- gateway-js/src/utilities/opentelemetry.ts
+++ gateway-js/src/utilities/opentelemetry.ts
@@ -1,4 +1,34 @@
 import opentelemetry from '@opentelemetry/api';
+import type { Attributes, Exception, Span } from '@opentelemetry/api';
+import type { GatewayGraphQLRequestContext } from '@apollo/server-gateway-interface';
+import { OperationContext } from '../operationContext';
+
+export type OpenTelemetryConfig = {
+  /**
+   * Whether or not to include the `graphql.document` attribute in the
+   * `gateway.request` OpenTelemetry span. When set to `true`, the attribute
+   * will contain the entire GraphQL document for the current request.
+   *
+   * Defaults to `false`, meaning that the GraphQL document will not be added
+   * as a span attribute.
+   */
+  includeDocument?: boolean;
+  /**
+   * Whether or not to record the GraphQL and internal errors that take place
+   * while processing a request as exception events in the OpenTelemetry spans
+   * in which they occur.
+   *
+   * When a number is given as a value, it represents the maximum number of
+   * exceptions that will be reported in each OpenTelemetry span.
+   *
+   * Regardless of the value of this setting, the span status code will be set
+   * to `ERROR` when a GraphQL or internal error occurs.
+   *
+   * Defaults to `false`, meaning that no exceptions will be reported in any
+   * spans.
+   */
+  recordExceptions?: boolean | number;
+}
 
 export enum OpenTelemetrySpanNames {
   REQUEST = 'gateway.request',
@@ -11,3 +41,68 @@ export enum OpenTelemetrySpanNames {
 
 const { name, version } = require('../../package.json');
 export const tracer = opentelemetry.trace.getTracer(`${name}/${version}`);
+
+export interface SpanAttributes extends Attributes {
+  /**
+   * @deprecated in favor of `graphql.operation.name`
+   */
+  operationName?: string;
+  'graphql.operation.name'?: string;
+  'graphql.operation.type'?: string;
+  'graphql.document'?: string;
+}
+
+export function requestContextSpanAttributes(
+  requestContext: GatewayGraphQLRequestContext,
+  config: OpenTelemetryConfig | undefined
+): SpanAttributes {
+  const spanAttributes: SpanAttributes = {};
+
+  if (requestContext.operationName) {
+    spanAttributes["operationName"] = requestContext.operationName;
+    spanAttributes["graphql.operation.name"] = requestContext.operationName;
+  }
+  if (config?.includeDocument && requestContext.source) {
+    spanAttributes["graphql.document"] = requestContext.source;
+  }
+
+  return spanAttributes;
+}
+
+export function operationContextSpanAttributes(
+  operationContext: OperationContext
+): SpanAttributes {
+  const spanAttributes: SpanAttributes = {};
+
+  if (operationContext.operation.operation) {
+    spanAttributes["graphql.operation.type"] = operationContext.operation.operation;
+  }
+
+  return spanAttributes;
+}
+
+export function recordExceptions(
+  span: Span,
+  exceptions: readonly Exception[],
+  config: OpenTelemetryConfig | undefined
+) {
+  const recordExceptions = config?.recordExceptions;
+
+  if (recordExceptions === undefined || recordExceptions === false) {
+    return;
+  }
+
+  let exceptionsToRecord;
+
+  if (recordExceptions === true) {
+    exceptionsToRecord = exceptions;
+  } else if (recordExceptions <= 0) {
+    return;
+  } else {
+    exceptionsToRecord = exceptions.slice(0, recordExceptions)
+  }
+
+  for (const exception of exceptionsToRecord) {
+    span.recordException(exception);
+  }
+}
