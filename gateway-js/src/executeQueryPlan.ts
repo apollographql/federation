1c2be3b8de937e88d3cb1ecbebc6412a043acec8
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2023-09-08 18:12:55 -0500

    fix: propagate type information when renaming entity fields

--- gateway-js/src/executeQueryPlan.ts
+++ gateway-js/src/executeQueryPlan.ts
@@ -508,6 +508,10 @@ async function executeFetch(
 
         for (let i = 0; i < entities.length; i++) {
           const receivedEntity = receivedEntities[i];
+          if (receivedEntity && !receivedEntity["__typename"]) {
+            const typename = representations[i].__typename;
+            receivedEntity["__typename"] = typename;
+          }
           applyRewrites(context.supergraphSchema, fetch.outputRewrites, receivedEntity);
           deepMerge(entities[representationToEntity[i]], receivedEntity);
         }
