80c1f5137db5b3fe55898210e36eb45e01bde206
Sylvain Lebresne <lebresne@gmail.com>
2023-09-14 11:08:11 +0200

    Fix reuse of existing __typename after entity fetch and ensure key renaming logic do nothing if the field to rename does not exists

--- gateway-js/src/executeQueryPlan.ts
+++ gateway-js/src/executeQueryPlan.ts
@@ -508,8 +508,9 @@ async function executeFetch(
 
         for (let i = 0; i < entities.length; i++) {
           const receivedEntity = receivedEntities[i];
+          const existingEntity = entities[representationToEntity[i]];
           if (receivedEntity && !receivedEntity["__typename"]) {
-            const typename = representations[i].__typename;
+            const typename = existingEntity["__typename"];
             receivedEntity["__typename"] = typename;
           }
           applyRewrites(context.supergraphSchema, fetch.outputRewrites, receivedEntity);
