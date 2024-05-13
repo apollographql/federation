f2dbdcf8b5a28b510ce9f42d2c95b6088e777b72
Dylan Anthony <dylan@apollographql.com>
2024-05-13 15:18:54 -0600

    Remove lsb

--- .codesandbox/install_rustup.sh
+++ .codesandbox/install_rustup.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 # Print out the architecture and Ubuntu version
 dpkg --print-architecture
-lsb_release -a
 
 curl http://http.us.debian.org/debian/pool/main/b/build-essential/build-essential_12.10_amd64.deb -o /tmp/build-essential_12.10_amd64.deb
 dpkg -i /tmp/build-essential_12.10_amd64.deb
