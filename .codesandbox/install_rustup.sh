08790f058b07079912bbac5b73b574130dabd92a
Dylan Anthony <dylan@apollographql.com>
2024-05-13 15:27:09 -0600

    Undo codesandbox customizations

--- .codesandbox/install_rustup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Print out the architecture and Ubuntu version
-dpkg --print-architecture
-
-curl http://http.us.debian.org/debian/pool/main/b/build-essential/build-essential_12.10_amd64.deb -o /tmp/build-essential_12.10_amd64.deb
-dpkg -i /tmp/build-essential_12.10_amd64.deb
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
