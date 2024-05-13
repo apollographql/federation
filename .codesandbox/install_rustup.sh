791f1340305f238e25c7eae3dda585813e70a26b
Dylan Anthony <dylan@apollographql.com>
2024-05-13 15:17:23 -0600

    sudo Install cc in codesandbox

--- .codesandbox/install_rustup.sh
+++ .codesandbox/install_rustup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-sudo apt-get install -y build-essential
+# Print out the architecture and Ubuntu version
+dpkg --print-architecture
+lsb_release -a
+
+curl http://http.us.debian.org/debian/pool/main/b/build-essential/build-essential_12.10_amd64.deb -o /tmp/build-essential_12.10_amd64.deb
+dpkg -i /tmp/build-essential_12.10_amd64.deb
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
