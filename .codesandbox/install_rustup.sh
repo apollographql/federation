72ce00c37cf3f060cdb1af325477481fc49c2496
Dylan Anthony <dylan@apollographql.com>
2024-05-13 15:04:06 -0600

    sudo Install cc in codesandbox

--- .codesandbox/install_rustup.sh
+++ .codesandbox/install_rustup.sh
@@ -1,20 +1,4 @@
 #!/usr/bin/env bash
 
-# Find out which operating system we're running
-if [ -f /etc/os-release ]; then
-  . /etc/os-release
-  OS=$ID
-elif [ -f /etc/debian_version ]; then
-  OS=debian
-elif [ -f /etc/fedora-release ]; then
-  OS=fedora
-elif [ -f /etc/centos-release ]; then
-  OS=centos
-else
-  echo "Unsupported operating system"
-  exit 1
-fi
-echo $OS
-
-apt-get install -y build-essential
+sudo apt-get install -y build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
