35e606533c34c1d6068bef90b23b267359ba3bc0
Dylan Anthony <dylan@apollographql.com>
2024-05-13 15:34:11 -0600

    Fix wasm publish (#3003)

--- internals-js/wasm/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Using a BASH script instead of build.rs because wasm-pack deadlocks when run
-# from a build.rs script: https://github.com/rustwasm/wasm-pack/issues/916
-
-cd $(dirname "$0")
-
-wasm-pack build --release --target nodejs --out-dir node
