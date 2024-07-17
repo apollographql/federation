bcf11d11a5f629a89ff8e02923d06e8531e7ab2d
Chris Lenfest <clenfest@apollographql.com>
2024-07-16 19:54:31 -0500

    Repro and first fix for @interfaceObject bug.
    We sometimes can get multiple paths generated for the same location (cause tbd). When this happens, it breaks the logic when detecting concrete type intersection as one type could potentially be an interfaceObject (and the check should not be run). To be safe, if the type we see does not exist in the list of possible implementation types, skip it from intersection testing.

--- composition-js/src/validate.ts
+++ composition-js/src/validate.ts
@@ -580,6 +580,9 @@ export class ValidationState {
         for (const { path } of newSubgraphPathInfos) {
           const subgraph = path.path.tail.source;
           const typeNames = possibleRuntimeTypeNamesSorted(path.path);
+          if (typeNames.length === 1 && !allRuntimeTypes.includes(typeNames[0])) {
+            continue;
+          }
           runtimeTypesPerSubgraphs.set(subgraph, typeNames);
           // Note: we're formatting the elements in `runtimeTYpesToSubgraphs` because we're going to use it if we display an error. This doesn't
           // impact our set equality though since the formatting is consistent betweeen elements and type names syntax is sufficiently restricted
