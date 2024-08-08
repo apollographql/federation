d9d8fb26272026e9f55c982ca6614deaf52204e9
Taylor Ninesling <taylor.ninesling@apollographql.com>
2024-08-08 11:07:32 -0500

    Run prettier:fix

--- internals-js/src/__tests__/schemaUpgrader.test.ts
+++ internals-js/src/__tests__/schemaUpgrader.test.ts
@@ -366,8 +366,8 @@ test('fully upgrades a schema with no @link directive', () => {
 
 test("don't add @shareable to subscriptions", () => {
   const subgraph1 = buildSubgraph(
-    "subgraph1",
-    "",
+    'subgraph1',
+    '',
     `#graphql
     type Query {
       hello: String
@@ -376,12 +376,12 @@ test("don't add @shareable to subscriptions", () => {
     type Subscription { 
       update: String!
     }
-  `
+  `,
   );
-  
+
   const subgraph2 = buildSubgraph(
-    "subgraph2",
-    "",
+    'subgraph2',
+    '',
     `#graphql
     type Query {
       hello: String
@@ -390,16 +390,30 @@ test("don't add @shareable to subscriptions", () => {
     type Subscription { 
       update: String!
     }
-  `
+  `,
   );
   const subgraphs = new Subgraphs();
   subgraphs.add(subgraph1);
   subgraphs.add(subgraph2);
   const result = upgradeSubgraphsIfNecessary(subgraphs);
 
-  expect(printSchema(result.subgraphs!.get("subgraph1")!.schema!)).not.toContain('update: String! @shareable');
-  expect(printSchema(result.subgraphs!.get("subgraph2")!.schema!)).not.toContain('update: String! @shareable');
-  
-  expect(result.subgraphs!.get("subgraph1")!.schema.type('Subscription')?.appliedDirectivesOf('@shareable').length).toBe(0);
-  expect(result.subgraphs!.get("subgraph2")!.schema.type('Subscription')?.appliedDirectivesOf('@shareable').length).toBe(0);
+  expect(
+    printSchema(result.subgraphs!.get('subgraph1')!.schema!),
+  ).not.toContain('update: String! @shareable');
+  expect(
+    printSchema(result.subgraphs!.get('subgraph2')!.schema!),
+  ).not.toContain('update: String! @shareable');
+
+  expect(
+    result
+      .subgraphs!.get('subgraph1')!
+      .schema.type('Subscription')
+      ?.appliedDirectivesOf('@shareable').length,
+  ).toBe(0);
+  expect(
+    result
+      .subgraphs!.get('subgraph2')!
+      .schema.type('Subscription')
+      ?.appliedDirectivesOf('@shareable').length,
+  ).toBe(0);
 });
