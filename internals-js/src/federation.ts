0d8fca1c8cc375bb8486f11f339984b69267417d
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2025-11-05 22:34:45 -0600

    fix: allow interface object fields to specify access control
    Update composition logic to allow specifying access control directives (`@authenticated`, `@requiresScopes` and `@policy`) on `@interfaceObject` fields.
    
    While we disallow access control on interface types and fields, we decided to support it on `@interfaceObject` as it is a useful pattern to define a single resolver (that may need access controls) for common interface fields. Alternative would require our users to explicitly define resolvers for all implementations which defeats the purpose of `@interfaceObject`.
    
    This PR refactors in how we propagate access control by providing additional merge sources when merging directives on interfaces, interface fields and object fields.

--- internals-js/src/federation.ts
+++ internals-js/src/federation.ts
@@ -1875,7 +1875,7 @@ export class FederationBlueprint extends SchemaBlueprint {
       validateSizedFieldsAreValidLists(application, parent, errorCollector);
     }
 
-    // Validate @authenticated, @requireScopes and @policy
+    // Validate @authenticated, @requireScopes and @policy usage on interfaces and interface objects
     validateNoAuthenticationOnInterfaces(metadata, errorCollector);
 
     return errorCollector;
@@ -2941,7 +2941,7 @@ function validateNoAuthenticationOnInterfaces(metadata: FederationMetadata, erro
         return isInterfaceType(type) || isInterfaceObjectType(baseType(type));
       }
       function isAppliedOnInterfaceField(elem: SchemaElement<any, any>) {
-        return isFieldDefinition(elem) && isAppliedOnInterface(elem.parent);
+        return isFieldDefinition(elem) && isInterfaceType(elem.parent);
       }
 
       if (isAppliedOnInterface(element) || isAppliedOnInterfaceField(element)) {
@@ -2958,7 +2958,7 @@ function validateNoAuthenticationOnInterfaces(metadata: FederationMetadata, erro
             break;
         }
         errorCollector.push(ERRORS.AUTHENTICATION_APPLIED_ON_INTERFACE.err(
-            `Invalid use of @${directive.name} on ${kind} "${element.coordinate}": @${directive.name} cannot be applied on interfaces, interface objects or their fields`,
+            `Invalid use of @${directive.name} on ${kind} "${element.coordinate}": @${directive.name} cannot be applied on interfaces, interface fields and interface objects`,
             {nodes: sourceASTs(application, element.parent)},
         ));
       }
